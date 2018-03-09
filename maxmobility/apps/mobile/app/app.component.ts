62c86317442b84c8c367402d8b360cc1a5c8445e
Brad Martin <bradwaynemartin@gmail.com>
2018-03-08 23:58:38 -0600

    init w/ app.component

--- maxmobility/apps/mobile/app/app.component.ts
+++ maxmobility/apps/mobile/app/app.component.ts
@@ -1,7 +1,27 @@
-import { Component } from "@angular/core";
+// angular
+import { Component } from '@angular/core';
+// nativescript
+import { registerElement } from 'nativescript-angular/element-registry';
+import { Kinvey } from 'kinvey-nativescript-sdk';
+// these modules don't have index.d.ts so TS compiler doesn't know their shape for importing
+const NS_CAROUSEL = require('nativescript-carousel');
+const NS_EXOPLAYER = require('nativescript-exoplayer');
+// app
+import { TNSKinveyService } from '@maxmobility/mobile';
+
+registerElement('Carousel', () => NS_CAROUSEL.Carousel);
+registerElement('CarouselItem', () => NS_CAROUSEL.CarouselItem);
+registerElement('exoplayer', () => NS_EXOPLAYER.ExoVideoPlayer);
 
 @Component({
-    selector: "ns-app",
-    templateUrl: "app.component.html"
+  selector: 'ns-app',
+  templateUrl: 'app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor() {
+    Kinvey.init({
+      appKey: `${TNSKinveyService.Kinvey_App_Key}`,
+      appSecret: `${TNSKinveyService.Kinvey_App_Secret}`
+    });
+  }
+}
--- maxmobility/apps/mobile/app/main.ts
+++ maxmobility/apps/mobile/app/main.ts
@@ -1,12 +1,8 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
-import { platformNativeScriptDynamic } from "nativescript-angular/platform";
+import { platformNativeScriptDynamic } from 'nativescript-angular/platform';
+import { AppModule } from './app.module';
 
-import { AppModule } from "./app.module";
-
-import { registerElement} from "nativescript-angular/element-registry";
-registerElement("VideoPlayer", () => require("nativescript-videoplayer").Video);
-registerElement("Carousel", () => require("nativescript-carousel").Carousel);
-registerElement("CarouselItem", () => require("nativescript-carousel").CarouselItem);
-registerElement("exoplayer", () => require("nativescript-exoplayer").Video);
+// import global stuff here
+import './async-await';
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
