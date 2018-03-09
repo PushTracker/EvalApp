251e570b372b7f60372c79f90f212df19f9a9082
Brad Martin <bradwaynemartin@gmail.com>
2018-03-08 23:57:27 -0600

    initial kinvey service

--- maxmobility/xplat/mobile/core/services/tns-kinvey.service.ts
+++ maxmobility/xplat/mobile/core/services/tns-kinvey.service.ts
@@ -1,14 +1,44 @@
-import { Injectable, Inject } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions, URLSearchParams } from '@angular/http';
 
-// import { KinveyService } from '@maxmobility/core';
-import * as TNSKinvey from 'kinvey-nativescript-sdk';
+// lib
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { validate } from 'email-validator';
+
+import * as Kinvey from 'kinvey-nativescript-sdk';
+import { User } from '@maxmobility/core';
 
 @Injectable()
 // export class TNSKinveyService extends KinveyService {
 export class TNSKinveyService {
-	constructor() {}
+  public static Kinvey_App_Key = 'kid_SyIIDJjdM';
+  public static Kinvey_App_Secret = '3cfe36e6ac8f4d80b04014cc980a4d47';
+  public static Kinvey_Host_Url = 'https://baas.kinvey.com/';
 
-	public upload(localPath: string, remotePath: string) {
-		// code to upload a file
-	}
+  constructor(private http: Http) {}
+
+  get user() {
+    return Kinvey.User.getActiveUser();
+  }
+
+  register(user: User) {
+    return Kinvey.User.signup(user);
+  }
+
+  login(username: string, pw: string) {
+    return Kinvey.User.login(username.trim(), pw.trim());
+  }
+
+  //   loginWithMIC() {
+  //     return TNSKinvey.Kinvey.User.loginWithMIC('http://redirecturi');
+  //   }
+
+  logoff() {
+    return Kinvey.User.logout();
+  }
+
+  resetPassword(email) {
+    return Kinvey.User.resetPassword(email);
+  }
 }
