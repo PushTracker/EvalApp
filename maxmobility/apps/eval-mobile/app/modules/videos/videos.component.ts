c02eb77de35869355a7f4f499aca6ae988531ec6
Devon Doebele <devon@max-mobility.com>
2018-04-10 16:08:37 -0700

    added videos to videos view

--- maxmobility/apps/eval-mobile/app/modules/videos/videos.component.ts
+++ maxmobility/apps/eval-mobile/app/modules/videos/videos.component.ts
@@ -1,12 +1,12 @@
 // angular
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 // nativescript
 import { LoadEventData, WebView } from 'tns-core-modules/ui/web-view';
 import { DrawerTransitionBase, SlideInOnTopTransition } from 'nativescript-ui-sidedrawer';
 import { RadSideDrawerComponent } from 'nativescript-ui-sidedrawer/angular';
 
-import {registerElement} from "nativescript-angular/element-registry";
-registerElement("exoplayer", () => require("nativescript-exoplayer").Video);
+// import {registerElement} from "nativescript-angular/element-registry";
+// registerElement("exoplayer", () => require("nativescript-exoplayer").Video);
 
 @Component({
   selector: 'Videos',
@@ -15,18 +15,37 @@ registerElement("exoplayer", () => require("nativescript-exoplayer").Video);
   styleUrls: ['./videos.component.css']
 })
 export class VideosComponent implements OnInit {
-  videos = [
-    { Url: 'https://drive.google.com/drive/folders/0B3EsLOeFJxiGX3p2MjloTGNyVFk', Description: 'SmartDrive MX2+ Basic Operation' },
-    { Url: 'https://drive.google.com/drive/folders/0B3EsLOeFJxiGX3p2MjloTGNyVFk', Description: 'PushTracker Basic Operation' },
-    { Url: 'https://drive.google.com/drive/folders/0B3EsLOeFJxiGX3p2MjloTGNyVFk', Description: 'Intro to the PushTracker App' },
-    { Url: 'https://drive.google.com/drive/folders/0B3EsLOeFJxiGX3p2MjloTGNyVFk', Description: 'Intro to the Eval App App' },
-    { Url: 'https://drive.google.com/drive/folders/0B3EsLOeFJxiGX3p2MjloTGNyVFk', Description: 'How to perform a Smart Drive Eval' },
-  ];
+
+
 
   // tslint:disable-next-line:max-line-length
-  videoHtmlString = '<iframe height="75" width="125" src="https://www.youtube.com/embed/6_M1J8HZXIk" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+  videoHtmlString_0 = '<iframe height="75" width="120" src="https://www.youtube.com/embed/hFid9ks551A" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+  videoHtmlString_1 = '<iframe height="75" width="130" src="https://www.youtube.com/embed/uhA3-svjQFg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+  videoHtmlString_2 = '<iframe height="75" width="130" src="https://www.youtube.com/embed/6_M1J8HZXIk" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+  videoHtmlString_3 = '<iframe height="75" width="130" src="https://www.youtube.com/embed/3B-6ked84us" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+  videoHtmlString_4 = '<iframe height="75" width="130" src="https://www.youtube.com/embed/3B-6ked84us" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+  videoHtmlString_5 = '<iframe height="75" width="130" src="https://www.youtube.com/embed/45Kj7zJpDcM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+  videoHtmlString_6 = '<iframe height="75" width="130" src="https://www.youtube.com/embed/hFid9ks551A" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+
+    videos = [
+    { Url: this.videoHtmlString_0, Description: 'SmartDrive Introduction' 
+    },
+    { Url: this.videoHtmlString_1, Description: 'SmartDrive MX2+ Basic Operation' },
+    { Url: this.videoHtmlString_2, Description: 'PushTracker Basic Operation' 
+    },
+    { Url: this.videoHtmlString_3, Description: 'Intro to the PushTracker App' 
+    },
+    { Url: this.videoHtmlString_4, Description: 'Intro to the Eval App App' 
+    },
+    { Url: this.videoHtmlString_5, Description: 'SmartDrive Evaluation and Training' 
+    },
+    { Url: this.videoHtmlString_6, Description: 'Interview with Chels and Steph' 
+    },
+    
+  ];          
 
   @ViewChild('drawer') drawerComponent: RadSideDrawerComponent;
+
 
   private _sideDrawerTransition: DrawerTransitionBase;
 
@@ -42,7 +61,16 @@ export class VideosComponent implements OnInit {
     this.drawerComponent.sideDrawer.showDrawer();
   }
 
-  onItemTapThirdList(args) {
+  didSelectItemAtIndex(args) {
+
+
+    
     console.log(args.index);
   }
+
+ didLoad(args) {
+   
+   console.log(args.index);
+ }
+
 }
