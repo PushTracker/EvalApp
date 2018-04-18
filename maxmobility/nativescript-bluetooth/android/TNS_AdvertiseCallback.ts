e0ec5537625e28afd8b853c9b8ec5369f1ad2620
Brad Martin <bradwaynemartin@gmail.com>
2018-04-18 15:43:15 -0500

    improve the onStartFailure for advertising

--- maxmobility/nativescript-bluetooth/android/TNS_AdvertiseCallback.ts
+++ maxmobility/nativescript-bluetooth/android/TNS_AdvertiseCallback.ts
@@ -34,15 +34,54 @@ export class TNS_AdvertiseCallback extends android.bluetooth.le.AdvertiseCallbac
    * Callback when advertising could not be started.
    * @param errorCode
    */
-  onStartFailure(errorCode) {
+  onStartFailure(errorCode: number) {
     CLog(CLogTypes.info, `---- TNS_AdvertiseCallback.onStartFailure ---- errorCode: ${errorCode}`);
+
+    // Get the error code value
+    let errorObj;
+    switch (errorCode) {
+      case 1:
+        // Failed to start advertising as the advertise data to be broadcasted is larger than 31 bytes.
+        errorObj = {
+          code: android.bluetooth.le.AdvertiseCallback.ADVERTISE_FAILED_DATA_TOO_LARGE,
+          msg: 'Failed to start advertising as the advertise data to be broadcasted is larger than 31 bytes.'
+        };
+        break;
+      case 2:
+        // Failed to start advertising because no advertising instance is available.
+        errorObj = {
+          code: android.bluetooth.le.AdvertiseCallback.ADVERTISE_FAILED_TOO_MANY_ADVERTISERS,
+          msg: 'Failed to start advertising because no advertising instance is available.'
+        };
+        break;
+      case 3:
+        // Failed to start advertising as the advertising is already started.
+        errorObj = {
+          code: android.bluetooth.le.AdvertiseCallback.ADVERTISE_FAILED_ALREADY_STARTED,
+          msg: 'Failed to start advertising as the advertising is already started.'
+        };
+        break;
+      case 4:
+        errorObj = {
+          code: android.bluetooth.le.AdvertiseCallback.ADVERTISE_FAILED_INTERNAL_ERROR,
+          msg: 'Operation failed due to an internal error.'
+        };
+        break;
+      case 5:
+        // This feature is not supported on this platform.
+        errorObj = {
+          code: android.bluetooth.le.AdvertiseCallback.ADVERTISE_FAILED_FEATURE_UNSUPPORTED,
+          msg: 'This feature is not supported on this platform.'
+        };
+        break;
+    }
 
     this.owner
       .get()
       .sendEvent(
         Bluetooth.bluetooth_advertise_failure_event,
-        { error: errorCode },
-        'TNS_AdvertiseCallback.onStartFailure'
+        { error: errorObj },
+        `TNS_AdvertiseCallback.onStartFailure --- error: ${errorObj.msg}`
       );
 
     // this.owner.get()._onBluetoothAdvertiseReject(errorCode);
