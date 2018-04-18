1bb813cc43b7fd28e5abdcc5f4d5d0be54e674fc
Brad Martin <bradwaynemartin@gmail.com>
2018-04-18 13:57:20 -0500

    clean up imports

--- maxmobility/apps/eval-mobile/app/modules/pairing/pairing.component.ts
+++ maxmobility/apps/eval-mobile/app/modules/pairing/pairing.component.ts
@@ -4,169 +4,167 @@ import { HttpClient } from '@angular/common/http';
 import { RouterExtensions } from 'nativescript-angular/router';
 // nativescript
 import { Progress } from 'tns-core-modules/ui/progress';
-import { ScrollView, ScrollEventData } from "ui/scroll-view";
-import { EventData } from 'data/observable';
-import { StackLayout } from 'ui/layouts/stack-layout';
-import { GridLayout } from 'ui/layouts/grid-layout';
+import { ScrollView, ScrollEventData } from 'tns-core-modules/ui/scroll-view';
+import { Observable, EventData } from 'tns-core-modules/data/observable';
+import { StackLayout } from 'tns-core-modules/ui/layouts/stack-layout';
+import { GridLayout } from 'tns-core-modules/ui/layouts/grid-layout';
 import { Color } from 'tns-core-modules/color';
-import { Image } from 'ui/image';
-import { Label } from 'ui/label';
-import { AnimationCurve } from "ui/enums";
-import { View } from "ui/core/view";
-import { Animation, AnimationDefinition } from "ui/animation";
+import { Image } from 'tns-core-modules/ui/image';
+import { Label } from 'tns-core-modules/ui/label';
+import { AnimationCurve } from 'tns-core-modules/ui/enums';
+import { View } from 'tns-core-modules/ui/core/view';
+import { Animation, AnimationDefinition } from 'tns-core-modules/ui/animation';
 import { DrawerTransitionBase, SlideInOnTopTransition } from 'nativescript-ui-sidedrawer';
 import { SnackBar, SnackBarOptions } from 'nativescript-snackbar';
 import { RadSideDrawerComponent } from 'nativescript-ui-sidedrawer/angular';
 // import { Observable, Scheduler } from "rxjs";
-import { Observable } from "data/observable";
 
 import { Pairing } from '@maxmobility/core';
 import { CLog, PairingService } from '@maxmobility/mobile';
 
 @Component({
-    selector: 'Pairing',
-    moduleId: module.id,
-    templateUrl: './pairing.component.html',
-    styleUrls: ['./pairing.component.css'],
-    providers: [PairingService]
+  selector: 'Pairing',
+  moduleId: module.id,
+  templateUrl: './pairing.component.html',
+  styleUrls: ['./pairing.component.css'],
+  providers: [PairingService]
 })
 export class PairingComponent implements OnInit, AfterViewInit {
-    @ViewChild('drawer') drawerComponent: RadSideDrawerComponent;
-    @ViewChild("ptImage") ptImage: ElementRef;
-    @ViewChild("sdImage") sdImage: ElementRef;
-    @ViewChild("title") title: ElementRef;
-    @ViewChild("description") description: ElementRef;
+  @ViewChild('drawer') drawerComponent: RadSideDrawerComponent;
+  @ViewChild('ptImage') ptImage: ElementRef;
+  @ViewChild('sdImage') sdImage: ElementRef;
+  @ViewChild('title') title: ElementRef;
+  @ViewChild('description') description: ElementRef;
 
-    show = false;
+  show = false;
 
-    pairing: Pairing[] = [];
+  pairing: Pairing[] = [];
 
-    private _sideDrawerTransition: DrawerTransitionBase;
+  private _sideDrawerTransition: DrawerTransitionBase;
 
-    constructor(private pairingService: PairingService, private _routerExtensions: RouterExtensions) {}
+  constructor(private pairingService: PairingService, private _routerExtensions: RouterExtensions) {}
 
-    ngOnInit() {
-	this.pairingService.getList().subscribe(res => {
-	    this.pairing = res;
-	});
+  ngOnInit() {
+    this.pairingService.getList().subscribe(res => {
+      this.pairing = res;
+    });
 
-	this._sideDrawerTransition = new SlideInOnTopTransition();
+    this._sideDrawerTransition = new SlideInOnTopTransition();
 
-	const title = <View>this.title.nativeElement;
-	const description = <View>this.description.nativeElement;
-	const pt = <View>this.ptImage.nativeElement as Image;
+    const title = <View>this.title.nativeElement;
+    const description = <View>this.description.nativeElement;
+    const pt = (<View>this.ptImage.nativeElement) as Image;
 
-	title.opacity = 0;
-	description.opacity = 0
+    title.opacity = 0;
+    description.opacity = 0;
+  }
 
-    }
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+    CLog('ngAfterViewInit');
 
-    ngAfterViewInit() {
+    const title = (<View>this.title.nativeElement) as Label;
+    const description = (<View>this.description.nativeElement) as Label;
+    const pt = (<View>this.ptImage.nativeElement) as Image;
 
-	console.log("ngAfterViewInit");
-	CLog("ngAfterViewInit");
+    title.opacity = 0;
+    title.text = 'PushTracker';
+    description.opacity = 0;
+    description.text = 'Press the right buton on the PushTracker to initiate pairing';
 
-	const title = <View>this.title.nativeElement as Label;
-	const description = <View>this.description.nativeElement as Label;
-	const pt = <View>this.ptImage.nativeElement as Image;
+    setTimeout(() => {
+      pt.animate({
+        duration: 1000,
+        curve: AnimationCurve.easeInOut,
+        opacity: 1
+      });
+    }, 500);
 
-	title.opacity = 0;
-	title.text = "PushTracker"
-	description.opacity = 0
-	description.text = "Press the right buton on the PushTracker to initiate pairing"
+    setTimeout(() => {
+      title.animate({
+        duration: 1000,
+        curve: AnimationCurve.easeInOut,
+        opacity: 1
+      });
+    }, 500);
 
-	setTimeout(() => {
-	    pt.animate({
-		duration: 1000,
-		curve: AnimationCurve.easeInOut,
-		opacity: 1
-	    });
-	}, 500)
+    setTimeout(() => {
+      description.animate({
+        duration: 1000,
+        curve: AnimationCurve.easeInOut,
+        opacity: 1
+      });
+    }, 750);
+  }
 
-	setTimeout(() => {
-            title.animate({
-		duration: 1000,
-		curve: AnimationCurve.easeInOut,
-		opacity: 1
-	    });
-	}, 500)
+  pageLoaded(args: EventData) {
+    let page = <GridLayout>args.object;
 
-	setTimeout(() => {
-            description.animate({
-		duration: 1000,
-		curve: AnimationCurve.easeInOut,
-		opacity: 1
-	    });
-	}, 750)
-	
+    console.log('pageLoaded');
+    CLog('pageLoaded');
+  }
 
-	
-    }
+  get sideDrawerTransition(): DrawerTransitionBase {
+    return this._sideDrawerTransition;
+  }
 
-    pageLoaded(args: EventData) {
-	let page = <GridLayout>args.object;
+  onDrawerButtonTap(): void {
+    this.drawerComponent.sideDrawer.showDrawer();
+  }
 
-	console.log("pageLoaded");
-	CLog("pageLoaded");
-    }
-
-    get sideDrawerTransition(): DrawerTransitionBase {
-	return this._sideDrawerTransition;
-    }
-
-    onDrawerButtonTap(): void {
-	this.drawerComponent.sideDrawer.showDrawer();
-    }
-
-    onConnectTap(args: EventData) {
-	const title = <View>this.title.nativeElement as Label;
-	const description = <View>this.description.nativeElement as Label;
-	const pt = <View>this.ptImage.nativeElement as Image;
-	const sd = <View>this.sdImage.nativeElement as Image;
-	pt.animate({
+  onConnectTap(args: EventData) {
+    const title = (<View>this.title.nativeElement) as Label;
+    const description = (<View>this.description.nativeElement) as Label;
+    const pt = (<View>this.ptImage.nativeElement) as Image;
+    const sd = (<View>this.sdImage.nativeElement) as Image;
+    pt
+      .animate({
+        duration: 1000,
+        curve: AnimationCurve.easeInOut,
+        opacity: 0
+      })
+      .then(() => {
+        setTimeout(() => {
+          sd.animate({
             duration: 1000,
             curve: AnimationCurve.easeInOut,
-            opacity: 0
-	}).then(() => {
-            setTimeout(() => {
-		sd.animate({
-		    duration: 1000,
-		    curve: AnimationCurve.easeInOut,
-		    opacity: 1
-		})
-            }, 500)
-	})
+            opacity: 1
+          });
+        }, 500);
+      });
 
-	title.animate({
+    title
+      .animate({
+        duration: 1000,
+        curve: AnimationCurve.easeInOut,
+        opacity: 0
+      })
+      .then(() => {
+        setTimeout(() => {
+          title.text = 'SmartDrive';
+          title.animate({
             duration: 1000,
             curve: AnimationCurve.easeInOut,
-            opacity: 0
-	}).then(() => {
-            setTimeout(() => {
-		title.text = "SmartDrive"
-		title.animate({
-		    duration: 1000,
-		    curve: AnimationCurve.easeInOut,
-		    opacity: 1
-		})
-            }, 500)
-	})
+            opacity: 1
+          });
+        }, 500);
+      });
 
-	description.animate({
+    description
+      .animate({
+        duration: 1000,
+        curve: AnimationCurve.easeInOut,
+        opacity: 0
+      })
+      .then(() => {
+        setTimeout(() => {
+          description.text = 'Press and hold the left button to enter settings.';
+          description.animate({
             duration: 1000,
             curve: AnimationCurve.easeInOut,
-            opacity: 0
-	}).then(() => {
-            setTimeout(() => {
-		description.text = "Press and hold the left button to enter settings."
-		description.animate({
-		    duration: 1000,
-		    curve: AnimationCurve.easeInOut,
-		    opacity: 1
-		})
-            }, 500)
-	})
-    }
-
-
+            opacity: 1
+          });
+        }, 500);
+      });
+  }
 }
