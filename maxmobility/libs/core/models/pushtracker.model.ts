b06cc171ec443e9400a3f4a50628a02964d7fdf4
William Emfinger <waemfinger@gmail.com>
2018-04-21 09:37:24 -0500

    continuing to flesh out the event interface for pt and sd.

--- maxmobility/libs/core/models/pushtracker.model.ts
+++ maxmobility/libs/core/models/pushtracker.model.ts
@@ -6,142 +6,144 @@ import { Packet } from '@maxmobility/core';
  * The options object passed to the PushTracker's performOTA function
  */
 export interface PTOTAOptions {
-  /**
-   * How long do we want our timeouts (generally on reconnection) to
-   * be in seconds?
-   */
-  timeout: number;
+    /**
+     * How long do we want our timeouts (generally on reconnection) to
+     * be in seconds?
+     */
+    timeout: number;
 
-  /**
-   * The array of bytes containing the firmware for the PT
-   */
-  firmware: number[];
+    /**
+     * The array of bytes containing the firmware for the PT
+     */
+    firmware: number[];
 }
 
 export class PushTracker extends Observable {
-  // Event names
-  public static pushtracker_disconnect_event = 'pushtracker_disconnect_event';
-  public static pushtracker_connect_event = 'pushtracker_disconnect_event';
+    // Event names
+    public static pushtracker_disconnect_event = 'pushtracker_disconnect_event';
+    public static pushtracker_connect_event = 'pushtracker_disconnect_event';
 
-  public static pushtracker_version_event = 'pushtracker_version_event';
-  public static pushtracker_error_event = 'pushtracker_error_event';
-  public static pushtracker_distance_event = 'pushtracker_distance_event';
-  public static pushtracker_settings_event = 'pushtracker_settings_event';
+    public static pushtracker_version_event = 'pushtracker_version_event';
+    public static pushtracker_error_event = 'pushtracker_error_event';
+    public static pushtracker_distance_event = 'pushtracker_distance_event';
+    public static pushtracker_settings_event = 'pushtracker_settings_event';
 
-  public static pushtracker_daily_info_event = 'pushtracker_daily_info_event';
-  public static pushtracker_awake_event = 'pushtracker_awake_event';
+    public static pushtracker_daily_info_event = 'pushtracker_daily_info_event';
+    public static pushtracker_awake_event = 'pushtracker_awake_event';
 
-  public static pushtracker_ota_timeout_event = 'pushtracker_ota_timeout_event';
-  public static pushtracker_ota_progress_event = 'pushtracker_ota_progress_event';
-  public static pushtracker_ota_version_event = 'pushtracker_ota_version_event';
-  public static pushtracker_ota_complete_event = 'pushtracker_ota_complete_event';
-  public static pushtracker_ota_failure_event = 'pushtracker_ota_failure_event';
+    public static pushtracker_ota_timeout_event = 'pushtracker_ota_timeout_event';
+    public static pushtracker_ota_progress_event = 'pushtracker_ota_progress_event';
+    public static pushtracker_ota_version_event = 'pushtracker_ota_version_event';
+    public static pushtracker_ota_complete_event = 'pushtracker_ota_complete_event';
+    public static pushtracker_ota_failure_event = 'pushtracker_ota_failure_event';
 
-  // public members
-  public version: number = 0xff; // firmware version number for the PT firmware
-  public mcu_version: number = 0xff; // firmware version number for the SD MCU firmware
-  public ble_version: number = 0xff; // firmware version number for the SD BLE firmware
-  public battery: number = 0; // battery percent Stat of Charge (SoC)
+    public events: any /*IPushTrackerEvents*/;
 
-  public address: string = ''; // MAC Address
+    // public members
+    public version: number = 0xff; // firmware version number for the PT firmware
+    public mcu_version: number = 0xff; // firmware version number for the SD MCU firmware
+    public ble_version: number = 0xff; // firmware version number for the SD BLE firmware
+    public battery: number = 0; // battery percent Stat of Charge (SoC)
 
-  // private members
+    public address: string = ''; // MAC Address
 
-  // functions
-  constructor(obj?: any) {
-    if (obj !== null && obj !== undefined) {
-      this.fromObject(obj);
+    // private members
+
+    // functions
+    constructor(obj?: any) {
+	if (obj !== null && obj !== undefined) {
+	    this.fromObject(obj);
+	}
     }
-  }
 
-  public data(): any {
-    return {
-      version: this.version,
-      address: this.address,
-      battery: this.battery
-    };
-  }
-
-  public fromObject(obj: any): void {
-    this.version = (obj && obj.version) || 0xff;
-    this.battery = (obj && obj.battery) || 0;
-    this.address = (obj && obj.address) || '';
-  }
-
-  // regular methods
-
-  public performOTA(otaOptions: PTOTAOptions) {
-    // TODO: handle all the ota process for this specific
-    // smartdrive
-  }
-
-  // handlers
-
-  public handleConnect() {
-    // TODO: update state and spawn events
-  }
-
-  public handleDisconnect() {
-    // TODO: update state and spawn events
-  }
-
-  public handlePacket(p: Packet) {
-    // TODO: determine type here and call the private handlers
-    // (which may update state or spawn events)
-    const packetType = p.Type();
-    const subType = p.SubType();
-    if (packetType && packetType == 'Data') {
-      switch (subType) {
-        case 'VersionInfo':
-          this.handleVersionInfo(p);
-          break;
-        case 'ErrorInfo':
-          this.handleErrorInfo(p);
-          break;
-        case 'TotalDistance':
-          this.handleDistance(p);
-          break;
-        case 'DailyInfo':
-          this.handleDailyInfo(p);
-          break;
-        case 'Ready':
-          this.handleReady(p);
-          break;
-        default:
-          break;
-      }
-    } else if (packetType && packetType == 'Command') {
-      switch (subType) {
-        case 'SetSettings':
-          this.handleSettings(p);
-          break;
-        default:
-          break;
-      }
+    public data(): any {
+	return {
+	    version: this.version,
+	    address: this.address,
+	    battery: this.battery
+	};
     }
-  }
 
-  // private functions
-  private handleVersionInfo(p: Packet) {
-    // This is sent by the PushTracker when it connects
-    const versionInfo = p.data('versionInfo');
-    /* Version Info
+    public fromObject(obj: any): void {
+	this.version = (obj && obj.version) || 0xff;
+	this.battery = (obj && obj.battery) || 0;
+	this.address = (obj && obj.address) || '';
+    }
+
+    // regular methods
+
+    public performOTA(otaOptions: PTOTAOptions) {
+	// TODO: handle all the ota process for this specific
+	// smartdrive
+    }
+
+    // handlers
+
+    public handleConnect() {
+	// TODO: update state and spawn events
+    }
+
+    public handleDisconnect() {
+	// TODO: update state and spawn events
+    }
+
+    public handlePacket(p: Packet) {
+	// TODO: determine type here and call the private handlers
+	// (which may update state or spawn events)
+	const packetType = p.Type();
+	const subType = p.SubType();
+	if (packetType && packetType == 'Data') {
+	    switch (subType) {
+		case 'VersionInfo':
+		    this.handleVersionInfo(p);
+		    break;
+		case 'ErrorInfo':
+		    this.handleErrorInfo(p);
+		    break;
+		case 'TotalDistance':
+		    this.handleDistance(p);
+		    break;
+		case 'DailyInfo':
+		    this.handleDailyInfo(p);
+		    break;
+		case 'Ready':
+		    this.handleReady(p);
+		    break;
+		default:
+		    break;
+	    }
+	} else if (packetType && packetType == 'Command') {
+	    switch (subType) {
+		case 'SetSettings':
+		    this.handleSettings(p);
+		    break;
+		default:
+		    break;
+	    }
+	}
+    }
+
+    // private functions
+    private handleVersionInfo(p: Packet) {
+	// This is sent by the PushTracker when it connects
+	const versionInfo = p.data('versionInfo');
+	/* Version Info
 	   struct {
 	   uint8_t     pushTracker;         // Major.Minor version as the MAJOR and MINOR nibbles of the byte.
 	   uint8_t     smartDrive;          // Major.Minor version as the MAJOR and MINOR nibbles of the byte.
 	   uint8_t     smartDriveBluetooth; // Major.Minor version as the MAJOR and MINOR nibbles of the byte.
 	   }            versionInfo;
 	*/
-    this.version = versionInfo.pushTracker;
-    this.mcu_version = versionInfo.smartDrive;
-    this.ble_version = versionInfo.smartDriveBluetooth;
-    // TODO: send version event to subscribers so they get updated
-  }
+	this.version = versionInfo.pushTracker;
+	this.mcu_version = versionInfo.smartDrive;
+	this.ble_version = versionInfo.smartDriveBluetooth;
+	// TODO: send version event to subscribers so they get updated
+    }
 
-  private handleErrorInfo(p: Packet) {
-    // This is sent by the PushTracker when it connects
-    const errorInfo = p.data('errorInfo');
-    /* Error Info
+    private handleErrorInfo(p: Packet) {
+	// This is sent by the PushTracker when it connects
+	const errorInfo = p.data('errorInfo');
+	/* Error Info
 	   struct {
 	   uint16_t            year;
 	   uint8_t             month;
@@ -158,30 +160,30 @@ export class PushTracker extends Observable {
 	   uint8_t             numBLEDisconnectErrors;
 	   }                     errorInfo;
 	*/
-    // TODO: send error event to subscribers so they get updated
-    // TODO: update error record for this pushtracker (locally and
-    // on the server)
-  }
+	// TODO: send error event to subscribers so they get updated
+	// TODO: update error record for this pushtracker (locally and
+	// on the server)
+    }
 
-  private handleDistance(p: Packet) {
-    // This is sent by the PushTracker when it connects
-    const distance = p.data('distanceInfo');
-    /* DistanceInfo
+    private handleDistance(p: Packet) {
+	// This is sent by the PushTracker when it connects
+	const distance = p.data('distanceInfo');
+	/* DistanceInfo
 	   struct {
            uint64_t   motorDistance;  /** Cumulative Drive distance in ticks.
            uint64_t   caseDistance;   /** Cumulative Case distance in ticks. 
 	   }            distanceInfo;
 	*/
-    // TODO: send distance event to subscribers so they get
-    // updated
-    // TODO: update distance record for this pushtracker (locally
-    // and on the server)
-  }
+	// TODO: send distance event to subscribers so they get
+	// updated
+	// TODO: update distance record for this pushtracker (locally
+	// and on the server)
+    }
 
-  private handleSettings(p: Packet) {
-    // This is sent by the PushTracker when it connects
-    const settings = p.data('settings');
-    /* Settings
+    private handleSettings(p: Packet) {
+	// This is sent by the PushTracker when it connects
+	const settings = p.data('settings');
+	/* Settings
 	   struct Settings {
 	   ControlMode controlMode;
 	   Units       units;
@@ -192,16 +194,16 @@ export class PushTracker extends Observable {
 	   float       maxSpeed;        /** Slider setting, range: [0.1, 1.0]
 	   } settings;
 	*/
-    // TODO: send settings event to subscribers so they get updated
-    // TODO: update our stored settings
-  }
+	// TODO: send settings event to subscribers so they get updated
+	// TODO: update our stored settings
+    }
 
-  private handleDailyInfo(p: Packet) {
-    // This is sent by the PushTracker every 10 seconds while it
-    // is connected (for today's daily info) - it also sends all
-    // unsent daily info for previous days on connection
-    const di = p.data('dailyInfo');
-    /* Daily Info
+    private handleDailyInfo(p: Packet) {
+	// This is sent by the PushTracker every 10 seconds while it
+	// is connected (for today's daily info) - it also sends all
+	// unsent daily info for previous days on connection
+	const di = p.data('dailyInfo');
+	/* Daily Info
 	   struct {
 	   uint16_t    year;
 	   uint8_t     month;
@@ -216,15 +218,38 @@ export class PushTracker extends Observable {
 	   uint8_t     sdBattery;       /** Percent, [0, 100].            
 	   }            dailyInfo;
 	*/
-    // TODO: send daily info event to subscribers so they get
-    // updated
-    // TODO: upate daily info record for this pushtracker (locally
-    // and on the server)
-  }
+	// TODO: send daily info event to subscribers so they get
+	// updated
+	// TODO: upate daily info record for this pushtracker (locally
+	// and on the server)
+    }
 
-  private handleReady(p: Packet) {
-    // This is sent by the PushTracker after it has received a
-    // Wake command
-    // TODO: send awawke event to subscribers so they get updated
-  }
+    private handleReady(p: Packet) {
+	// This is sent by the PushTracker after it has received a
+	// Wake command
+	// TODO: send awawke event to subscribers so they get updated
+    }
+}
+
+/**
+ * All of the events for PushTracker that can be emitted and listened
+ * to.
+ */
+export interface IPushtrackerEvents {
+    pushtracker_disconnect_event: string;
+    pushtracker_connect_event: string;
+
+    pushtracker_version_event: string;
+    pushtracker_error_event: string;
+    pushtracker_distance_event: string;
+    pushtracker_settings_event: string;
+
+    pushtracker_daily_info_event: string;
+    pushtracker_awake_event: string;
+
+    pushtracker_ota_timeout_event: string;
+    pushtracker_ota_progress_event: string;
+    pushtracker_ota_version_event: string;
+    pushtracker_ota_complete_event: string;
+    pushtracker_ota_failure_event: string;
 }
--- maxmobility/libs/core/models/smartdrive.model.ts
+++ maxmobility/libs/core/models/smartdrive.model.ts
@@ -6,136 +6,138 @@ import { Packet } from '@maxmobility/core';
  * The options object passed to the SmartDrive's performOTA function
  */
 export interface SDOTAOptions {
-  /**
-   * The type of device we want to OTA - can be 'MCU', 'BLE', or 'BOTH'
-   */
-  deviceType: string;
+    /**
+     * The type of device we want to OTA - can be 'MCU', 'BLE', or 'BOTH'
+     */
+    deviceType: string;
 
-  /**
-   * How long do we want our timeouts (generally on reconnection) to
-   * be in seconds?
-   */
-  timeout: number;
+    /**
+     * How long do we want our timeouts (generally on reconnection) to
+     * be in seconds?
+     */
+    timeout: number;
 
-  /**
-   * The array of bytes containing the firmware for the MCU
-   * (including header)
-   */
-  mcuFirmware: number[];
+    /**
+     * The array of bytes containing the firmware for the MCU
+     * (including header)
+     */
+    mcuFirmware: number[];
 
-  /**
-   * The array of bytes containing the firmware for the BLE
-   * (including header)
-   */
-  bleFirmware: number[];
+    /**
+     * The array of bytes containing the firmware for the BLE
+     * (including header)
+     */
+    bleFirmware: number[];
 }
 
 export class SmartDrive extends Observable {
-  // Event names
-  public static smartdrive_disconnect_event = 'smartdrive_disconnect_event';
-  public static smartdrive_connect_event = 'smartdrive_disconnect_event';
+    // Event names
+    public static smartdrive_disconnect_event = 'smartdrive_disconnect_event';
+    public static smartdrive_connect_event = 'smartdrive_disconnect_event';
 
-  public static smartdrive_ble_version_event = 'smartdrive_ble_version_event';
-  public static smartdrive_mcu_version_event = 'smartdrive_mcu_version_event';
+    public static smartdrive_ble_version_event = 'smartdrive_ble_version_event';
+    public static smartdrive_mcu_version_event = 'smartdrive_mcu_version_event';
 
-  public static smartdrive_ota_timeout_event = 'smartdrive_ota_timeout_event';
-  public static smartdrive_ota_progress_event = 'smartdrive_ota_progress_event';
-  public static smartdrive_ota_version_event = 'smartdrive_ota_version_event';
-  public static smartdrive_ota_complete_event = 'smartdrive_ota_complete_event';
-  public static smartdrive_ota_failure_event = 'smartdrive_ota_failure_event';
+    public static smartdrive_ota_timeout_event = 'smartdrive_ota_timeout_event';
+    public static smartdrive_ota_progress_event = 'smartdrive_ota_progress_event';
+    public static smartdrive_ota_version_event = 'smartdrive_ota_version_event';
+    public static smartdrive_ota_complete_event = 'smartdrive_ota_complete_event';
+    public static smartdrive_ota_failure_event = 'smartdrive_ota_failure_event';
 
-  // public members
-  public mcu_version: number = 0xff; // microcontroller firmware version number
-  public ble_version: number = 0xff; // bluetooth chip firmware version number
-  public battery: number = 0; // battery percent Stat of Charge (SoC)
-  public driveDistance: number = 0; // cumulative total distance the smartDrive has driven
-  public coastDistance: number = 0; // cumulative total distance the smartDrive has gone
+    public events: any /*ISmartDriveEvents*/;
 
-  public address: string = ''; // MAC Address
+    // public members
+    public mcu_version: number = 0xff; // microcontroller firmware version number
+    public ble_version: number = 0xff; // bluetooth chip firmware version number
+    public battery: number = 0; // battery percent Stat of Charge (SoC)
+    public driveDistance: number = 0; // cumulative total distance the smartDrive has driven
+    public coastDistance: number = 0; // cumulative total distance the smartDrive has gone
 
-  // private members
+    public address: string = ''; // MAC Address
 
-  // functions
-  constructor(obj?: any) {
-    if (obj !== null && obj !== undefined) {
-      this.fromObject(obj);
+    // private members
+
+    // functions
+    constructor(obj?: any) {
+	if (obj !== null && obj !== undefined) {
+	    this.fromObject(obj);
+	}
     }
-  }
 
-  public data(): any {
-    return {
-      mcu_version: this.mcu_version,
-      ble_version: this.ble_version,
-      battery: this.battery,
-      address: this.address
-    };
-  }
-
-  public fromObject(obj: any): void {
-    this.mcu_version = (obj && obj.mcu_version) || 0xff;
-    this.ble_version = (obj && obj.ble_version) || 0xff;
-    this.battery = (obj && obj.battery) || 0;
-    this.address = (obj && obj.address) || '';
-  }
-
-  // regular methods
-
-  public performOTA(otaOptions: SDOTAOptions) {
-    // TODO: handle all the ota process for this specific
-    // smartdrive
-  }
-
-  // handlers
-
-  public handleConnect() {
-    // TODO: update state and spawn events
-  }
-
-  public handleDisconnect() {
-    // TODO: update state and spawn events
-  }
-
-  public handlePacket(p: Packet) {
-    const packetType = p.Type();
-    const subType = p.SubType();
-    if (packetType && packetType == 'Data') {
-      switch (subType) {
-        case 'DeviceInfo':
-          this.handleDeviceInfo(p);
-          break;
-        case 'MotorInfo':
-          this.handleMotorInfo(p);
-          break;
-        case 'DistanceInfo':
-          this.handleDistanceInfo(p);
-          break;
-        default:
-          break;
-      }
+    public data(): any {
+	return {
+	    mcu_version: this.mcu_version,
+	    ble_version: this.ble_version,
+	    battery: this.battery,
+	    address: this.address
+	};
     }
-  }
 
-  // private functions
-  private handleDeviceInfo(p: Packet) {
-    // This is sent by the SmartDrive Bluetooth Chip when it
-    // connects
-    const devInfo = p.data('deviceInfo');
-    // so they get updated
-    /* Device Info
+    public fromObject(obj: any): void {
+	this.mcu_version = (obj && obj.mcu_version) || 0xff;
+	this.ble_version = (obj && obj.ble_version) || 0xff;
+	this.battery = (obj && obj.battery) || 0;
+	this.address = (obj && obj.address) || '';
+    }
+
+    // regular methods
+
+    public performOTA(otaOptions: SDOTAOptions) {
+	// TODO: handle all the ota process for this specific
+	// smartdrive
+    }
+
+    // handlers
+
+    public handleConnect() {
+	// TODO: update state and spawn events
+    }
+
+    public handleDisconnect() {
+	// TODO: update state and spawn events
+    }
+
+    public handlePacket(p: Packet) {
+	const packetType = p.Type();
+	const subType = p.SubType();
+	if (packetType && packetType == 'Data') {
+	    switch (subType) {
+		case 'DeviceInfo':
+		    this.handleDeviceInfo(p);
+		    break;
+		case 'MotorInfo':
+		    this.handleMotorInfo(p);
+		    break;
+		case 'DistanceInfo':
+		    this.handleDistanceInfo(p);
+		    break;
+		default:
+		    break;
+	    }
+	}
+    }
+
+    // private functions
+    private handleDeviceInfo(p: Packet) {
+	// This is sent by the SmartDrive Bluetooth Chip when it
+	// connects
+	const devInfo = p.data('deviceInfo');
+	// so they get updated
+	/* Device Info
 	   struct {
 	   Device     device;     // Which Device is this about?
 	   uint8_t    version;    // Major.Minor version as the MAJOR and MINOR nibbles of the byte.
 	   }            deviceInfo;
 	*/
-    this.ble_version = devInfo.version;
-    // TODO: send version event (for BLE_VERSION) to subscribers
-  }
+	this.ble_version = devInfo.version;
+	// TODO: send version event (for BLE_VERSION) to subscribers
+    }
 
-  private handleMotorInfo(p: Packet) {
-    // This is sent by the SmartDrive microcontroller every 200ms
-    // (5 hz) while connected
-    const motorInfo = p.data('motorInfo');
-    /* Motor Info
+    private handleMotorInfo(p: Packet) {
+	// This is sent by the SmartDrive microcontroller every 200ms
+	// (5 hz) while connected
+	const motorInfo = p.data('motorInfo');
+	/* Motor Info
 	   struct {
 	   Motor::State state;
 	   uint8_t      batteryLevel; // [0,100] integer percent. 
@@ -146,24 +148,42 @@ export class SmartDrive extends Observable {
 	   float        driveTime;
 	   }            motorInfo;
 	*/
-    this.mcu_version = motorInfo.version;
-    this.battery = motorInfo.batteryLevel;
-    // TODO: send version event (for MCU_VERSION) to subscribers
-    // so they get updated about this smartDrive's version
-    // TODO: update state (is the motor on or off)
-  }
+	this.mcu_version = motorInfo.version;
+	this.battery = motorInfo.batteryLevel;
+	// TODO: send version event (for MCU_VERSION) to subscribers
+	// so they get updated about this smartDrive's version
+	// TODO: update state (is the motor on or off)
+    }
 
-  private handleDistanceInfo(p: Packet) {
-    // This is sent by the SmartDrive microcontroller every 1000
-    // ms (1 hz) while connected and the motor is off
-    const distInfo = p.data('distanceInfo');
-    /* Distance Info
+    private handleDistanceInfo(p: Packet) {
+	// This is sent by the SmartDrive microcontroller every 1000
+	// ms (1 hz) while connected and the motor is off
+	const distInfo = p.data('distanceInfo');
+	/* Distance Info
 	   struct {
            uint64_t   motorDistance;  // Cumulative Drive distance in ticks.
            uint64_t   caseDistance;   // Cumulative Case distance in ticks.
 	   }            distanceInfo;
 	*/
-    this.driveDistance = distInfo.motorDistance;
-    this.coastDistance = distInfo.caseDistance;
-  }
+	this.driveDistance = distInfo.motorDistance;
+	this.coastDistance = distInfo.caseDistance;
+    }
+}
+
+/**
+ * All of the events for SmartDrive that can be emitted and listened
+ * to.
+ */
+export interface ISmartDriveEvents {
+    smartdrive_disconnect_event: string;
+    smartdrive_connect_event: string;
+
+    smartdrive_ble_version_event: string;
+    smartdrive_mcu_version_event: string;
+
+    smartdrive_ota_timeout_event: string;
+    smartdrive_ota_progress_event: string;
+    smartdrive_ota_version_event: string;
+    smartdrive_ota_complete_event: string;
+    smartdrive_ota_failure_event: string;
 }
