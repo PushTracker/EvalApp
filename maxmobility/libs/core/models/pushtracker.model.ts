1f847e4fa6014cc65cfc26a948311fa899047bbd
William Emfinger <waemfinger@gmail.com>
2018-04-23 09:01:29 -0500

    properly call super in derived classes

--- maxmobility/libs/core/models/pushtracker.model.ts
+++ maxmobility/libs/core/models/pushtracker.model.ts
@@ -6,165 +6,164 @@ import { Packet } from '@maxmobility/core';
  * The options object passed to the PushTracker's performOTA function
  */
 export interface PTOTAOptions {
-    /**
-     * How long do we want our timeouts (generally on reconnection) to
-     * be in seconds?
-     */
-    timeout: number;
+  /**
+   * How long do we want our timeouts (generally on reconnection) to
+   * be in seconds?
+   */
+  timeout: number;
 
-    /**
-     * The array of bytes containing the firmware for the PT
-     */
-    firmware: number[];
+  /**
+   * The array of bytes containing the firmware for the PT
+   */
+  firmware: number[];
 }
 
 export class PushTracker extends Observable {
-    // Event names
-    public static pushtracker_disconnect_event = 'pushtracker_disconnect_event';
-    public static pushtracker_connect_event = 'pushtracker_disconnect_event';
+  // Event names
+  public static pushtracker_disconnect_event = 'pushtracker_disconnect_event';
+  public static pushtracker_connect_event = 'pushtracker_disconnect_event';
 
-    public static pushtracker_version_event = 'pushtracker_version_event';
-    public static pushtracker_error_event = 'pushtracker_error_event';
-    public static pushtracker_distance_event = 'pushtracker_distance_event';
-    public static pushtracker_settings_event = 'pushtracker_settings_event';
+  public static pushtracker_version_event = 'pushtracker_version_event';
+  public static pushtracker_error_event = 'pushtracker_error_event';
+  public static pushtracker_distance_event = 'pushtracker_distance_event';
+  public static pushtracker_settings_event = 'pushtracker_settings_event';
 
-    public static pushtracker_daily_info_event = 'pushtracker_daily_info_event';
-    public static pushtracker_awake_event = 'pushtracker_awake_event';
+  public static pushtracker_daily_info_event = 'pushtracker_daily_info_event';
+  public static pushtracker_awake_event = 'pushtracker_awake_event';
 
-    public static pushtracker_ota_timeout_event = 'pushtracker_ota_timeout_event';
-    public static pushtracker_ota_progress_event = 'pushtracker_ota_progress_event';
-    public static pushtracker_ota_version_event = 'pushtracker_ota_version_event';
-    public static pushtracker_ota_complete_event = 'pushtracker_ota_complete_event';
-    public static pushtracker_ota_failure_event = 'pushtracker_ota_failure_event';
+  public static pushtracker_ota_timeout_event = 'pushtracker_ota_timeout_event';
+  public static pushtracker_ota_progress_event = 'pushtracker_ota_progress_event';
+  public static pushtracker_ota_version_event = 'pushtracker_ota_version_event';
+  public static pushtracker_ota_complete_event = 'pushtracker_ota_complete_event';
+  public static pushtracker_ota_failure_event = 'pushtracker_ota_failure_event';
 
-    public events: any /*IPushTrackerEvents*/;
+  public events: any /*IPushTrackerEvents*/;
 
-    // public members
-    public version: number = 0xff; // firmware version number for the PT firmware
-    public mcu_version: number = 0xff; // firmware version number for the SD MCU firmware
-    public ble_version: number = 0xff; // firmware version number for the SD BLE firmware
-    public battery: number = 0; // battery percent Stat of Charge (SoC)
+  // public members
+  public version: number = 0xff; // firmware version number for the PT firmware
+  public mcu_version: number = 0xff; // firmware version number for the SD MCU firmware
+  public ble_version: number = 0xff; // firmware version number for the SD BLE firmware
+  public battery: number = 0; // battery percent Stat of Charge (SoC)
 
-    public address: string = ''; // MAC Address
+  public address: string = ''; // MAC Address
 
-    // private members
+  // private members
 
-    // functions
-    constructor(obj?: any) {
-        if (obj !== null && obj !== undefined) {
-            this.fromObject(obj);
-        }
+  // functions
+  constructor(obj?: any) {
+    super();
+    if (obj !== null && obj !== undefined) {
+      this.fromObject(obj);
     }
+  }
 
-    public data(): any {
-        return {
-            version: this.version,
-            address: this.address,
-            battery: this.battery
-        };
+  public data(): any {
+    return {
+      version: this.version,
+      address: this.address,
+      battery: this.battery
+    };
+  }
+
+  public fromObject(obj: any): void {
+    this.version = (obj && obj.version) || 0xff;
+    this.battery = (obj && obj.battery) || 0;
+    this.address = (obj && obj.address) || '';
+  }
+
+  // regular methods
+
+  /**
+   * Notify events by name and optionally pass data
+   */
+  public sendEvent(eventName: string, data?: any, msg?: string) {
+    this.notify({
+      eventName,
+      object: this,
+      data,
+      message: msg
+    });
+  }
+
+  public performOTA(otaOptions: PTOTAOptions) {
+    // TODO: handle all the ota process for this specific
+    // smartdrive
+  }
+
+  // handlers
+
+  public handleConnect() {
+    // TODO: update state and spawn events
+    this.sendEvent(PushTracker.pushtracker_connect_event);
+  }
+
+  public handleDisconnect() {
+    // TODO: update state and spawn events
+    this.sendEvent(PushTracker.pushtracker_disconnect_event);
+  }
+
+  public handlePacket(p: Packet) {
+    // TODO: determine type here and call the private handlers
+    // (which may update state or spawn events)
+    const packetType = p.Type();
+    const subType = p.SubType();
+    if (packetType && packetType == 'Data') {
+      switch (subType) {
+        case 'VersionInfo':
+          this.handleVersionInfo(p);
+          break;
+        case 'ErrorInfo':
+          this.handleErrorInfo(p);
+          break;
+        case 'TotalDistance':
+          this.handleDistance(p);
+          break;
+        case 'DailyInfo':
+          this.handleDailyInfo(p);
+          break;
+        case 'Ready':
+          this.handleReady(p);
+          break;
+        default:
+          break;
+      }
+    } else if (packetType && packetType == 'Command') {
+      switch (subType) {
+        case 'SetSettings':
+          this.handleSettings(p);
+          break;
+        default:
+          break;
+      }
     }
+  }
 
-    public fromObject(obj: any): void {
-        this.version = (obj && obj.version) || 0xff;
-        this.battery = (obj && obj.battery) || 0;
-        this.address = (obj && obj.address) || '';
-    }
-
-    // regular methods
-
-    /**
-     * Notify events by name and optionally pass data
-     */
-    public sendEvent(eventName: string, data?: any, msg?: string) {
-	this.notify({
-	    eventName,
-	    object: this,
-	    data,
-	    message: msg
-	});
-    }
-
-    public performOTA(otaOptions: PTOTAOptions) {
-        // TODO: handle all the ota process for this specific
-        // smartdrive
-    }
-
-    // handlers
-
-    public handleConnect() {
-        // TODO: update state and spawn events
-	this.sendEvent(PushTracker.pushtracker_connect_event);
-    }
-
-    public handleDisconnect() {
-        // TODO: update state and spawn events
-	this.sendEvent(PushTracker.pushtracker_disconnect_event);
-    }
-
-    public handlePacket(p: Packet) {
-        // TODO: determine type here and call the private handlers
-        // (which may update state or spawn events)
-        const packetType = p.Type();
-        const subType = p.SubType();
-        if (packetType && packetType == 'Data') {
-            switch (subType) {
-                case 'VersionInfo':
-                    this.handleVersionInfo(p);
-                    break;
-                case 'ErrorInfo':
-                    this.handleErrorInfo(p);
-                    break;
-                case 'TotalDistance':
-                    this.handleDistance(p);
-                    break;
-                case 'DailyInfo':
-                    this.handleDailyInfo(p);
-                    break;
-                case 'Ready':
-                    this.handleReady(p);
-                    break;
-                default:
-                    break;
-            }
-        } else if (packetType && packetType == 'Command') {
-            switch (subType) {
-                case 'SetSettings':
-                    this.handleSettings(p);
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    // private functions
-    private handleVersionInfo(p: Packet) {
-        // This is sent by the PushTracker when it connects
-        const versionInfo = p.data('versionInfo');
-        /* Version Info
+  // private functions
+  private handleVersionInfo(p: Packet) {
+    // This is sent by the PushTracker when it connects
+    const versionInfo = p.data('versionInfo');
+    /* Version Info
            struct {
            uint8_t     pushTracker;         // Major.Minor version as the MAJOR and MINOR nibbles of the byte.
            uint8_t     smartDrive;          // Major.Minor version as the MAJOR and MINOR nibbles of the byte.
            uint8_t     smartDriveBluetooth; // Major.Minor version as the MAJOR and MINOR nibbles of the byte.
            }            versionInfo;
         */
-        this.version = versionInfo.pushTracker;
-        this.mcu_version = versionInfo.smartDrive;
-        this.ble_version = versionInfo.smartDriveBluetooth;
-        // TODO: send version event to subscribers so they get updated
-	this.sendEvent(
-	    PushTracker.pushtracker_version_event,
-	    {
-		pt: this.version,
-		mcu: this.mcu_version,
-		ble: this.ble_version
-	    });
-    }
+    this.version = versionInfo.pushTracker;
+    this.mcu_version = versionInfo.smartDrive;
+    this.ble_version = versionInfo.smartDriveBluetooth;
+    // TODO: send version event to subscribers so they get updated
+    this.sendEvent(PushTracker.pushtracker_version_event, {
+      pt: this.version,
+      mcu: this.mcu_version,
+      ble: this.ble_version
+    });
+  }
 
-    private handleErrorInfo(p: Packet) {
-        // This is sent by the PushTracker when it connects
-        const errorInfo = p.data('errorInfo');
-        /* Error Info
+  private handleErrorInfo(p: Packet) {
+    // This is sent by the PushTracker when it connects
+    const errorInfo = p.data('errorInfo');
+    /* Error Info
            struct {
            uint16_t            year;
            uint8_t             month;
@@ -181,41 +180,37 @@ export class PushTracker extends Observable {
            uint8_t             numBLEDisconnectErrors;
            }                     errorInfo;
         */
-        // TODO: send error event to subscribers so they get updated
-	this.sendEvent(
-	    PushTracker.pushtracker_error_event,
-	    {
-		// what should we put here?
-	    });
-        // TODO: update error record for this pushtracker (locally and
-        // on the server)
-    }
+    // TODO: send error event to subscribers so they get updated
+    this.sendEvent(PushTracker.pushtracker_error_event, {
+      // what should we put here?
+    });
+    // TODO: update error record for this pushtracker (locally and
+    // on the server)
+  }
 
-    private handleDistance(p: Packet) {
-        // This is sent by the PushTracker when it connects
-        const distance = p.data('distanceInfo');
-        /* DistanceInfo
+  private handleDistance(p: Packet) {
+    // This is sent by the PushTracker when it connects
+    const distance = p.data('distanceInfo');
+    /* DistanceInfo
            struct {
            uint64_t   motorDistance;  /** Cumulative Drive distance in ticks.
            uint64_t   caseDistance;   /** Cumulative Case distance in ticks. 
            }            distanceInfo;
         */
-        // TODO: send distance event to subscribers so they get
-        // updated
-	this.sendEvent(
-	    PushTracker.pushtracker_distance_event,
-	    {
-		driveDistance: distance.motorDistance, // TODO: make sure these accessors work
-		coastDistance: distance.caseDistance   //  - may need to use p.getMotorDistance() etc.
-	    });
-        // TODO: update distance record for this pushtracker (locally
-        // and on the server)
-    }
+    // TODO: send distance event to subscribers so they get
+    // updated
+    this.sendEvent(PushTracker.pushtracker_distance_event, {
+      driveDistance: distance.motorDistance, // TODO: make sure these accessors work
+      coastDistance: distance.caseDistance //  - may need to use p.getMotorDistance() etc.
+    });
+    // TODO: update distance record for this pushtracker (locally
+    // and on the server)
+  }
 
-    private handleSettings(p: Packet) {
-        // This is sent by the PushTracker when it connects
-        const settings = p.data('settings');
-        /* Settings
+  private handleSettings(p: Packet) {
+    // This is sent by the PushTracker when it connects
+    const settings = p.data('settings');
+    /* Settings
            struct Settings {
            ControlMode controlMode;
            Units       units;
@@ -226,21 +221,19 @@ export class PushTracker extends Observable {
            float       maxSpeed;        /** Slider setting, range: [0.1, 1.0]
            } settings;
         */
-        // TODO: send settings event to subscribers so they get updated
-	this.sendEvent(
-	    PushTracker.pushtracker_settings_event,
-	    {
-		// TODO: fill in this
-	    });
-        // TODO: update our stored settings
-    }
+    // TODO: send settings event to subscribers so they get updated
+    this.sendEvent(PushTracker.pushtracker_settings_event, {
+      // TODO: fill in this
+    });
+    // TODO: update our stored settings
+  }
 
-    private handleDailyInfo(p: Packet) {
-        // This is sent by the PushTracker every 10 seconds while it
-        // is connected (for today's daily info) - it also sends all
-        // unsent daily info for previous days on connection
-        const di = p.data('dailyInfo');
-        /* Daily Info
+  private handleDailyInfo(p: Packet) {
+    // This is sent by the PushTracker every 10 seconds while it
+    // is connected (for today's daily info) - it also sends all
+    // unsent daily info for previous days on connection
+    const di = p.data('dailyInfo');
+    /* Daily Info
            struct {
            uint16_t    year;
            uint8_t     month;
@@ -255,23 +248,21 @@ export class PushTracker extends Observable {
            uint8_t     sdBattery;       /** Percent, [0, 100].            
            }            dailyInfo;
         */
-        // TODO: send daily info event to subscribers so they get
-        // updated
-	this.sendEvent(
-	    PushTracker.pushtracker_daily_info_event,
-	    {
-		// TODO: fill this in
-	    });
-        // TODO: upate daily info record for this pushtracker (locally
-        // and on the server)
-    }
+    // TODO: send daily info event to subscribers so they get
+    // updated
+    this.sendEvent(PushTracker.pushtracker_daily_info_event, {
+      // TODO: fill this in
+    });
+    // TODO: upate daily info record for this pushtracker (locally
+    // and on the server)
+  }
 
-    private handleReady(p: Packet) {
-        // This is sent by the PushTracker after it has received a
-        // Wake command
-        // TODO: send awawke event to subscribers so they get updated
-	this.sendEvent(PushTracker.pushtracker_awake_event);
-    }
+  private handleReady(p: Packet) {
+    // This is sent by the PushTracker after it has received a
+    // Wake command
+    // TODO: send awawke event to subscribers so they get updated
+    this.sendEvent(PushTracker.pushtracker_awake_event);
+  }
 }
 
 /**
@@ -279,20 +270,20 @@ export class PushTracker extends Observable {
  * to.
  */
 export interface IPushtrackerEvents {
-    pushtracker_disconnect_event: string;
-    pushtracker_connect_event: string;
+  pushtracker_disconnect_event: string;
+  pushtracker_connect_event: string;
 
-    pushtracker_version_event: string;
-    pushtracker_error_event: string;
-    pushtracker_distance_event: string;
-    pushtracker_settings_event: string;
+  pushtracker_version_event: string;
+  pushtracker_error_event: string;
+  pushtracker_distance_event: string;
+  pushtracker_settings_event: string;
 
-    pushtracker_daily_info_event: string;
-    pushtracker_awake_event: string;
+  pushtracker_daily_info_event: string;
+  pushtracker_awake_event: string;
 
-    pushtracker_ota_timeout_event: string;
-    pushtracker_ota_progress_event: string;
-    pushtracker_ota_version_event: string;
-    pushtracker_ota_complete_event: string;
-    pushtracker_ota_failure_event: string;
+  pushtracker_ota_timeout_event: string;
+  pushtracker_ota_progress_event: string;
+  pushtracker_ota_version_event: string;
+  pushtracker_ota_complete_event: string;
+  pushtracker_ota_failure_event: string;
 }
