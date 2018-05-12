3e9fb4a3365fc911d7220089db444096f49c79ed
Devon Doebele <devon@max-mobility.com>
2018-05-12 10:45:40 -0700

    added descriptions to side-drawer items and refined the layout a bit more

--- maxmobility/apps/eval-mobile/app/modules/faq/faq.component.ts
+++ maxmobility/apps/eval-mobile/app/modules/faq/faq.component.ts
@@ -43,9 +43,6 @@ export class FAQComponent implements OnInit {
 
     private _sideDrawerTransition: DrawerTransitionBase;
 
-    /************************************************************
-     * Use the sideDrawerTransition property to change the open/close animation of the drawer.
-     *************************************************************/
     ngOnInit(): void {
 	this._sideDrawerTransition = new SlideInOnTopTransition();
     }
@@ -54,10 +51,6 @@ export class FAQComponent implements OnInit {
 	return this._sideDrawerTransition;
     }
 
-    /************************************************************
-     * According to guidelines, if you have a drawer on your page, you should always
-     * have a button that opens it. Use the showDrawer() function to open the app drawer section.
-     *************************************************************/
     onDrawerButtonTap(): void {
 	this.drawerComponent.sideDrawer.showDrawer();
     }
