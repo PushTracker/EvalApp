b7f088cfc90ae00e48fa7fe159c4f48d9922df6f
William Emfinger <waemfinger@gmail.com>
2018-05-22 09:41:21 -0500

    continuing to flesh out and refine the trial data storage and interface.

--- maxmobility/apps/eval-mobile/app/modules/trial/trial.component.ts
+++ maxmobility/apps/eval-mobile/app/modules/trial/trial.component.ts
@@ -8,23 +8,34 @@ import { SegmentedBar, SegmentedBarItem } from 'tns-core-modules/ui/segmented-ba
 import { TextField } from 'tns-core-modules/ui/text-field';
 import { confirm } from 'tns-core-modules/ui/dialogs';
 import * as switchModule from 'tns-core-modules/ui/switch';
-import { Observable } from 'tns-core-modules/data/observable';
-import { ObservableArray, ChangedData, ChangeType } from 'tns-core-modules/data/observable-array';
+import { Observable, fromObject } from 'tns-core-modules/data/observable';
 
 // app
 import { ProgressService } from '@maxmobility/mobile';
 import { SnackBar, SnackBarOptions } from 'nativescript-snackbar';
 import { Packet, DailyInfo, PushTracker, SmartDrive } from '@maxmobility/core';
-import { EvaluationService, BluetoothService } from '@maxmobility/mobile';
+import { BluetoothService } from '@maxmobility/mobile';
 
-export class Trial extends Observable {
+export class Trial {
+  name: string = '';
+  // questionnaire
+  flat: boolean = false;
+  ramap: boolean = false;
+  inclines: boolean = false;
+  other: boolean = false;
+  // settings
+  max_speed: number = 0.7;
+  acceleration: number = 0.3;
+  // metrics
   distance: number = 0;
+  // with SD
   with_pushes: number = 0;
   with_coast: number = 0;
   with_distance: number = 0;
   with_start: Date;
   with_end: Date;
   with_elapsed: number = 0;
+  // without SD
   without_pushes: number = 0;
   without_coast: number = 0;
   without_distance: number = 0;
@@ -43,8 +54,8 @@ export class TrialComponent implements OnInit {
   // NON STATIC:
   @ViewChild('drawer') drawerComponent: RadSideDrawerComponent;
 
-  trialName: string = '';
-  trial: Trial = new Trial();
+  trial: Observable = fromObject(new Trial());
+
   startedWith: boolean = false;
   startedWithout: boolean = false;
   finishedWith: boolean = false;
@@ -96,9 +107,16 @@ export class TrialComponent implements OnInit {
       // let user know we're doing something
       this._progressService.show('Starting Trial');
       // set up handlers
+      const trialStartedHandler = () => {
+        this._progressService.hide();
+        this.trial.set('with_start', new Date());
+        pt.off(PushTracker.pushtracker_distance_event, distanceHandler);
+        pt.off(PushTracker.pushtracker_daily_info_event, dailyInfoHandler);
+        this.startedWith = true;
+      };
       const dailyInfoHandler = data => {
         // get the data
-        this.trial.with_pushes = data.data.pushesWithout + data.data.pushesWith;
+        this.trial.set('with_pushes', data.data.pushesWithout + data.data.pushesWith);
         // record that we've gotten it
         haveDailyInfo = true;
         if (haveDailyInfo && haveDistance) {
@@ -107,19 +125,12 @@ export class TrialComponent implements OnInit {
       };
       const distanceHandler = data => {
         // get the data
-        this.trial.distance = PushTracker.caseTicksToMiles(data.data.coastDistance);
+        this.trial.set('distance', PushTracker.caseTicksToMiles(data.data.coastDistance));
         // record that we've gotten it
         haveDistance = true;
         if (haveDailyInfo && haveDistance) {
           trialStartedHandler();
         }
-      };
-      const trialStartedHandler = () => {
-        this._progressService.hide();
-        this.trial.with_start = new Date();
-        pt.off(PushTracker.pushtracker_distance_event, distanceHandler);
-        pt.off(PushTracker.pushtracker_daily_info_event, dailyInfoHandler);
-        this.startedWith = true;
       };
       // send command to get distance:
       pt.sendPacket('Command', 'DistanceRequest');
@@ -147,9 +158,21 @@ export class TrialComponent implements OnInit {
         // let user know we're doing something
         this._progressService.show('Stopping Trial');
         // set up handlers
+        const trialStoppedHandler = () => {
+          this.finishedWith = true;
+          this.trial.set('with_end', new Date());
+          this.trial.set(
+            'with_elapsed',
+            (this.trial.get('with_end').getTime() - this.trial.get('with_start').getTime()) / 60000
+          ); // diff is in ms
+          this.trial.set('with_coast', this.trial.get('with_elapsed') * 60 / this.trial.get('with_pushes'));
+          pt.off(PushTracker.pushtracker_distance_event, distanceHandler);
+          pt.off(PushTracker.pushtracker_daily_info_event, dailyInfoHandler);
+          this._progressService.hide();
+        };
         const dailyInfoHandler = data => {
           // get the data
-          this.trial.with_pushes = data.data.pushesWithout + data.data.pushesWith - this.trial.with_pushes;
+          this.trial.set('with_pushes', data.data.pushesWithout + data.data.pushesWith - this.trial.get('with_pushes'));
           // record that we've gotten it
           haveDailyInfo = true;
           if (haveDailyInfo && haveDistance) {
@@ -158,22 +181,16 @@ export class TrialComponent implements OnInit {
         };
         const distanceHandler = data => {
           // get the data
-          this.trial.distance = PushTracker.caseTicksToMiles(data.data.coastDistance) - this.trial.distance;
-          this.trial.distance = this.trial.distance * 1604; // convert to meters
+          this.trial.set(
+            'distance',
+            PushTracker.caseTicksToMiles(data.data.coastDistance) - this.trial.get('distance')
+          );
+          this.trial.set('distance', this.trial.get('distance') * 1604); // convert to meters
           // record that we've gotten it
           haveDistance = true;
           if (haveDailyInfo && haveDistance) {
             trialStoppedHandler();
           }
-        };
-        const trialStoppedHandler = () => {
-          this.finishedWith = true;
-          this.trial.with_end = new Date();
-          this.trial.with_elapsed = (this.trial.with_end.getTime() - this.trial.with_start.getTime()) / 60000; // diff is in ms
-          this.trial.with_coast = this.trial.with_pushes / (this.trial.with_elapsed * 60);
-          pt.off(PushTracker.pushtracker_distance_event, distanceHandler);
-          pt.off(PushTracker.pushtracker_daily_info_event, dailyInfoHandler);
-          this._progressService.hide();
         };
         // send command to get distance:
         pt.sendPacket('Command', 'DistanceRequest');
@@ -200,17 +217,17 @@ export class TrialComponent implements OnInit {
       // let user know we're doing something
       this._progressService.show('Starting Trial');
       // set up handlers
-      const dailyInfoHandler = data => {
-        // get the data
-        this.trial.without_pushes = data.data.pushesWithout + data.data.pushesWith;
-        // record that we've gotten it
-        trialStartedHandler();
-      };
       const trialStartedHandler = () => {
         this._progressService.hide();
         this.startedWithout = true;
-        this.trial.without_start = new Date();
+        this.trial.set('without_start', new Date());
         pt.off(PushTracker.pushtracker_daily_info_event, dailyInfoHandler);
+      };
+      const dailyInfoHandler = data => {
+        // get the data
+        this.trial.set('without_pushes', data.data.pushesWithout + data.data.pushesWith);
+        // record that we've gotten it
+        trialStartedHandler();
       };
       pt.on(PushTracker.pushtracker_daily_info_event, dailyInfoHandler);
     }
@@ -232,19 +249,25 @@ export class TrialComponent implements OnInit {
         // let user know we're doing something
         this._progressService.show('Stopping Trial');
         // set up handlers
-        const dailyInfoHandler = data => {
-          // get the data
-          this.trial.without_pushes = data.data.pushesWithout + data.data.pushesWith - this.trial.without_pushes;
-          // record that we've gotten it
-          trialStoppedHandler();
-        };
         const trialStoppedHandler = () => {
           this.finishedWithout = true;
-          this.trial.without_end = new Date();
-          this.trial.without_elapsed = (this.trial.without_end.getTime() - this.trial.without_start.getTime()) / 60000; // diff is in ms
-          this.trial.without_coast = this.trial.without_pushes / (this.trial.without_elapsed * 60);
+          this.trial.set('without_end', new Date());
+          this.trial.set(
+            'without_elapsed',
+            (this.trial.get('without_end').getTime() - this.trial.get('without_start').getTime()) / 60000
+          ); // diff is in ms
+          this.trial.set('without_coast', this.trial.get('without_elapsed') * 60 / this.trial.get('without_pushes'));
           pt.off(PushTracker.pushtracker_daily_info_event, dailyInfoHandler);
           this._progressService.hide();
+        };
+        const dailyInfoHandler = data => {
+          // get the data
+          this.trial.set(
+            'without_pushes',
+            data.data.pushesWithout + data.data.pushesWith - this.trial.get('without_pushes')
+          );
+          // record that we've gotten it
+          trialStoppedHandler();
         };
         // wait for push / coast data and distance:
         pt.on(PushTracker.pushtracker_daily_info_event, dailyInfoHandler);
@@ -254,29 +277,11 @@ export class TrialComponent implements OnInit {
   }
 
   onTextChange(args) {
-    const textField = <TextField>args.object;
-
-    console.log('onTextChange');
-    this.trialName = textField.text;
-  }
-
-  onReturn(args) {
-    const textField = <TextField>args.object;
-
-    console.log('onReturn');
-    this.trialName = textField.text;
-  }
-
-  showAlert(result) {
-    alert('Text: ' + result);
-  }
-
-  submit(result) {
-    alert('Text: ' + result);
+    this.trial.set('name', args.value);
   }
 
   onSliderUpdate(key, args) {
-    this.settings.set(key, args.object.value);
+    this.trial.set(key, args.object.value / 10);
   }
 
   ngOnInit(): void {
@@ -285,10 +290,6 @@ export class TrialComponent implements OnInit {
 
   get sideDrawerTransition(): DrawerTransitionBase {
     return this._sideDrawerTransition;
-  }
-
-  get settings(): Observable {
-    return EvaluationService.settings;
   }
 
   onDrawerButtonTap(): void {
