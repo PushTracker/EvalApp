681caf5ae5aafc0707b11efbc7227eb0540205f3
Brad Martin <bradwaynemartin@gmail.com>
2018-07-18 09:42:49 -0500

    closes #126

--- maxmobility/xplat/mobile/core/services/logging.service.ts
+++ maxmobility/xplat/mobile/core/services/logging.service.ts
@@ -1,3 +1,6 @@
+import { Injectable } from '@angular/core';
+import { Sentry, SentryOptions } from 'nativescript-sentry';
+
 export class LoggingUtil {
   public static debug = true;
 }
@@ -8,21 +11,41 @@ export const CLog = (...args) => {
   }
 };
 
-// tslint:disable-next-line:max-classes-per-file
+@Injectable()
 export class LoggingService {
+  constructor() {}
   /**
    * Will console.log() the error argument. If devmode is false then we capture
    * the exception with Sentry logging.
    * @param err
    */
-  logException(exception: Error) {
+  public logException(exception: Error, options: SentryOptions = {}) {
     console.log(exception);
 
-    // wire up to some service/api
+    // Sentry.setContextUser({
+    //   email: this._userService.user.email,
+    //   id: this._userService.user._id
+    // });
+
+    // const extras: SentryOptions = {
+    //   extra: {
+    //     user: {
+    //       email: this._userService.user.email,
+    //       id: this._userService.user._id
+    //     }
+    //   }
+    // };
+
+    // if error type
+    if (exception instanceof Error) {
+      Sentry.captureException(exception, options);
+    } else {
+      Sentry.captureMessage(exception as any, options);
+    }
   }
 
-  logMessage(message: string) {
+  public logMessage(message: string, options: SentryOptions = {}) {
     console.log(message);
-    // wire up to service/api
+    Sentry.captureMessage(message, options);
   }
 }
