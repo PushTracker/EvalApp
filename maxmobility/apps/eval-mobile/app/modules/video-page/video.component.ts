1789e64ded8f39bd39472a76c1482902f1d8c20f
William Emfinger <waemfinger@gmail.com>
2018-07-19 09:50:22 -0500

    closes #134

--- maxmobility/apps/eval-mobile/app/modules/video-page/video.component.ts
+++ maxmobility/apps/eval-mobile/app/modules/video-page/video.component.ts
@@ -1,24 +1,8 @@
-// angular
-import { Component, ElementRef, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { RouterExtensions } from 'nativescript-angular/router';
-// nativescript
-import { Progress } from 'tns-core-modules/ui/progress';
-import { ScrollView, ScrollEventData } from 'ui/scroll-view';
-import { EventData } from 'data/observable';
-import { StackLayout } from 'ui/layouts/stack-layout';
-import { GridLayout } from 'ui/layouts/grid-layout';
-import { Color } from 'tns-core-modules/color';
-import { Image } from 'ui/image';
-import { Label } from 'ui/label';
-import { AnimationCurve } from 'ui/enums';
-import { View } from 'ui/core/view';
-import { Animation, AnimationDefinition } from 'ui/animation';
-import { SnackBar, SnackBarOptions } from 'nativescript-snackbar';
-import { Observable } from 'data/observable';
-
-const orientation = require('nativescript-orientation');
+import * as orientation from 'nativescript-orientation';
+import { YoutubePlayer } from 'nativescript-youtubeplayer';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'Video',
@@ -26,17 +10,14 @@ const orientation = require('nativescript-orientation');
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.css']
 })
-export class VideoComponent implements OnInit, AfterViewInit {
-  @ViewChild('description') description: ElementRef;
-
-  url = String();
+export class VideoComponent implements OnInit {
   title = String();
   desc = String();
   options = { rel: 1 };
 
-  private routeSub: any; // subscription to route observer
+  private routeSub: Subscription; // subscription to route observer
 
-  constructor(private _routerExtensions: RouterExtensions, private router: Router, private route: ActivatedRoute) {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
     // see https://github.com/NativeScript/nativescript-angular/issues/1049
@@ -46,39 +27,23 @@ export class VideoComponent implements OnInit, AfterViewInit {
         orientation.disableRotation();
       }
     });
-    //orientation.setOrientation('portrait');
+    // orientation.setOrientation('portrait');
     orientation.enableRotation();
-
-    const query = this.route.snapshot.queryParams;
-
-    this.url = `${query['url']}`;
-    this.title = `${query['title']}`;
-    this.desc = `${query['desc']}`;
   }
 
-  ngAfterViewInit() {
-    // const title = <View>this.title.nativeElement as Label;
-    // const pt = <View>this.ptImage.nativeElement as Image;
-    // setTimeout(() => {
-    //     pt.animate({
-    // 	duration: 1000,
-    // 	curve: AnimationCurve.easeInOut,
-    // 	opacity: 1
-    //     });
-    // }, 500)
-    // setTimeout(() => {
-    //         title.animate({
-    // 	duration: 1000,
-    // 	curve: AnimationCurve.easeInOut,
-    // 	opacity: 1
-    //     });
-    // }, 500)
-    // setTimeout(() => {
-    //         description.animate({
-    // 	duration: 1000,
-    // 	curve: AnimationCurve.easeInOut,
-    // 	opacity: 1
-    //     });
-    // }, 750)
+  /**
+   * Set the video src when the player is loaded.
+   * @param args
+   */
+  playerLoaded(args) {
+    const player = args.object as YoutubePlayer;
+    const query = this.route.snapshot.queryParams;
+
+    // Set the title and video description from the queryParams of current route
+    this.title = `${query.title}`;
+    this.desc = `${query.desc}`;
+
+    // Set the player src for the video
+    player.src = `${query.url}`;
   }
 }
