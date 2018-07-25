145712d7e12a63c60fba8a17cea3a4e70b5c7c72
Brad Martin <bradwaynemartin@gmail.com>
2018-07-25 12:36:38 -0500

    closes #147

--- maxmobility/apps/eval-mobile/app/modules/demo-detail/demo-detail.module.ts
+++ maxmobility/apps/eval-mobile/app/modules/demo-detail/demo-detail.module.ts
@@ -1,10 +1,10 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { Routes } from '@angular/router';
-import { NativeScriptRouterModule } from 'nativescript-angular/router';
-import { DemoDetailComponent } from './demo-detail.component';
-import { SharedModule } from '../shared/shared.module';
 import { TranslateModule } from '@ngx-translate/core';
+import { NativeScriptRouterModule } from 'nativescript-angular/router';
 import { BarcodeScanner } from 'nativescript-barcodescanner';
+import { SharedModule } from '../shared/shared.module';
+import { DemoDetailComponent } from './demo-detail.component';
 
 const routes: Routes = [{ path: '', component: DemoDetailComponent }];
 
