e7070f5b482128b09fc02d85a67851472689ccfc
Brad Martin <bradmartin@Maxs-iMac.local>
2018-08-02 13:36:33 -0500

    WIP #144 - images working iOS profile locally between app runs

--- maxmobility/apps/eval-mobile/app/modules/account/account.module.ts
+++ maxmobility/apps/eval-mobile/app/modules/account/account.module.ts
@@ -1,9 +1,9 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { Routes } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 import { NativeScriptRouterModule } from 'nativescript-angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { AccountComponent } from './account.component';
-import { TranslateModule } from '@ngx-translate/core';
 
 const routes: Routes = [{ path: '', component: AccountComponent }];
 
