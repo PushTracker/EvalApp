c26cd46ff78d82e6d3498d93c9ea855d34d0b1e6
Brad Martin <bradwaynemartin@gmail.com>
2018-09-26 17:10:23 -0500

    picker plugin usage for date/time

--- maxmobility/apps/eval-mobile/app/modules/evals/evals.component.ts
+++ maxmobility/apps/eval-mobile/app/modules/evals/evals.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Evaluation } from '@maxmobility/core';
 import { EvaluationService } from '@maxmobility/mobile';
 import { alert } from 'tns-core-modules/ui/dialogs/dialogs';
+import { SearchBar } from 'tns-core-modules/ui/search-bar';
 import { TranslateService } from '@ngx-translate/core';
+import { ModalDatetimepicker } from 'nativescript-modal-datetimepicker';
 
 @Component({
   selector: 'Evals',
@@ -21,6 +23,8 @@ export class EvalsComponent implements OnInit {
    * Used to hide the activity indicator in UI and show the list.
    */
   evalsLoaded = false;
+
+  private _picker = new ModalDatetimepicker();
 
   constructor(private _evalService: EvaluationService, private _translateService: TranslateService) {}
 
@@ -41,6 +45,36 @@ export class EvalsComponent implements OnInit {
         okButtonText: this._translateService.instant('dialogs.ok')
       });
     }
+  }
+
+  onSearchTap() {
+    console.log('onSearchTap');
+    this._picker
+      .pickDate({
+        title: 'Select Your Birthday',
+        theme: 'light',
+        maxDate: new Date(),
+        is24HourView: false
+      })
+      .then((result: any) => {
+        // Note the month is 1-12 (unlike js which is 0-11)
+        console.log('Date is: ' + result.day + '-' + result.month + '-' + result.year);
+        const jsdate = new Date(result.year, result.month - 1, result.day);
+        console.log(jsdate);
+      })
+      .catch(error => {
+        console.log('Error: ' + error);
+      });
+  }
+
+  onSubmit(args) {
+    const searchBar = args.object as SearchBar;
+    alert('You are searching for ' + searchBar.text);
+  }
+
+  onTextChanged(args) {
+    const searchBar = args.object as SearchBar;
+    console.log('SearchBar text changed! New value: ' + searchBar.text);
   }
 
   onEvalItemTap(event) {
