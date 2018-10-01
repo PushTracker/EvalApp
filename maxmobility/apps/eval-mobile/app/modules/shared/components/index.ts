093903dcae94936dddee572db5a82998d2e17190
Brad Martin <bradwaynemartin@gmail.com>
2018-10-01 12:08:12 -0500

    delete old drawer components

--- maxmobility/apps/eval-mobile/app/modules/shared/components/index.ts
+++ maxmobility/apps/eval-mobile/app/modules/shared/components/index.ts
@@ -1,4 +1,5 @@
-import { MyDrawerComponent } from './my-drawer/my-drawer.component';
-import { MyDrawerItemComponent } from './my-drawer-item/my-drawer-item.component';
+// import { MyDrawerComponent } from './my-drawer/my-drawer.component';
+// import { MyDrawerItemComponent } from './my-drawer-item/my-drawer-item.component';
 
-export const SHARED_COMPONENTS: any[] = [MyDrawerComponent, MyDrawerItemComponent];
+// export const SHARED_COMPONENTS: any[] = [MyDrawerComponent, MyDrawerItemComponent];
+export const SHARED_COMPONENTS: any[] = [];
