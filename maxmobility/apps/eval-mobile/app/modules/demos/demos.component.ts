57dfb8a74550b86df6fc6330f07f8d0742525299
Brad Martin <bradwaynemartin@gmail.com>
2018-10-02 15:59:37 -0500

    WIP : change location of demo unit 👻

--- maxmobility/apps/eval-mobile/app/modules/demos/demos.component.ts
+++ maxmobility/apps/eval-mobile/app/modules/demos/demos.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { Demo } from '@maxmobility/core';
-import { DemoService, FirmwareService } from '@maxmobility/mobile';
+import { Demo, UserTypes } from '@maxmobility/core';
+import { DemoService, FirmwareService, LocationService, LoggingService } from '@maxmobility/mobile';
+import { TranslateService } from '@ngx-translate/core';
+import { Kinvey } from 'kinvey-nativescript-sdk';
 import { RouterExtensions } from 'nativescript-angular/router';
-import { BarcodeScanner } from 'nativescript-barcodescanner';
-import { isAndroid, isIOS } from 'platform';
+import * as geolocation from 'nativescript-geolocation';
+import { SnackBar } from 'nativescript-snackbar';
 import { ObservableArray } from 'tns-core-modules/data/observable-array';
+import { isAndroid, isIOS } from 'tns-core-modules/platform';
+import { clearTimeout, setTimeout } from 'tns-core-modules/timer/timer';
+import { action, confirm } from 'tns-core-modules/ui/dialogs';
 
 @Component({
   selector: 'Demos',
@@ -17,11 +22,15 @@ export class DemosComponent {
     return DemoService.Demos;
   }
 
+  private _snackBar = new SnackBar();
+  private _datastore = Kinvey.DataStore.collection<any>('SmartDrives');
+
   constructor(
-    private barcodeScanner: BarcodeScanner,
     private routerExtensions: RouterExtensions,
     private _demoService: DemoService,
-    private _firmwareService: FirmwareService
+    private _firmwareService: FirmwareService,
+    private _logService: LoggingService,
+    private _translateService: TranslateService
   ) {}
 
   isIOS(): boolean {
@@ -45,18 +54,93 @@ export class DemosComponent {
     });
   }
 
-  onNavBtnTap(): void {
-    this.routerExtensions.navigate(['/home'], {
-      // clearHistory: true,
-      transition: {
-        name: 'slideRight'
-      }
-    });
-  }
-
   addDemo() {
     // add a new demo
     console.log('add a new demo');
     this.routerExtensions.navigate(['/demo-detail'], {});
+  }
+
+  /**
+   * Confirm if user wants to update the location of the demo unit to the current location (and tell them what it is).
+   * @param demo [Demo] - Demo unit to update.
+   */
+  async onUpdateLocationButtonTap(demo: Demo) {
+    let processTimeout = 0;
+
+    try {
+      console.log(demo);
+
+      const isEnabled = await geolocation.isEnabled();
+      if (isEnabled) {
+        // if more than 750ms pass then show a snackbar that location is being calculated...
+        processTimeout = setTimeout(() => {
+          this._snackBar.simple(this._translateService.instant('demos.location-calculating'));
+        }, 750);
+      }
+
+      const loc = await LocationService.getLocationData();
+      // clear the timeout when done
+      clearTimeout(processTimeout);
+
+      console.log('current location', loc);
+
+      // confirm with user if they want to update the demo location
+      const result = await confirm({
+        message: `${this._translateService.instant('demos.location-confirm-message')} ${loc.place_name}?`,
+        okButtonText: this._translateService.instant('dialogs.yes'),
+        neutralButtonText: this._translateService.instant('dialogs.no')
+      });
+
+      if (result === true) {
+        // update the demo units location 👍
+        console.log('need to update demo now');
+        demo.location = loc.place_name;
+        demo.geo = [loc.longitude, loc.latitude];
+        this._datastore.save(demo);
+      }
+    } catch (error) {
+      // clear the timeout when done
+      clearTimeout(processTimeout);
+      this._logService.logException(error);
+    }
+  }
+
+  /**
+   * show list actions for clinicians
+   * 1. handing over a demo unit to a clinician
+   * 2. retrieving a demo unit from a clinician
+   * 3. requesting a demo unit from nearby reps
+   */
+
+  async onDemoActionsButtonTap(demo: Demo) {
+    // clinician user type options
+    const cOpts = ['Request Demo from Rep.'];
+    // rep user type options
+    const rOpts = ['Handing Demo to Clinician', 'Retrieve Demo from Clinician'];
+    console.log('Kinvey.User.getActiveUser().type', Kinvey.User.getActiveUser().data.type);
+
+    const result = await action({
+      message: 'Demo Unit Actions',
+      actions: Kinvey.User.getActiveUser().data.type === UserTypes.Representative ? rOpts : cOpts,
+      cancelable: true,
+      cancelButtonText: 'Cancel'
+    });
+
+    // if no action selected
+    if (!result) {
+      return;
+    }
+
+    switch (result) {
+      case 'Handing Demo to Clinician':
+        console.log('handing to clinician');
+        break;
+      case 'Retrieve Demo from Clinician':
+        console.log('retrieve from clinician');
+        break;
+      case 'Request Demo from Rep':
+        console.log('request demo from rep');
+        break;
+    }
   }
 }
