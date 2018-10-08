728bbc4e2a88c5a263c7b470c0f04277c76d09bc
William Emfinger <waemfinger@gmail.com>
2018-10-08 16:19:28 -0500

    fixed crash when navigating away from training before snackbar shows. updated styling of training, home, and pairing.

--- maxmobility/apps/eval-mobile/app/modules/home/home.component.ts
+++ maxmobility/apps/eval-mobile/app/modules/home/home.component.ts
@@ -64,7 +64,7 @@ export class HomeComponent {
     },
     {
       Image: '~/assets/images/trial-history-transparent.png',
-      Description: 'Evals',
+      Description: 'menu.eval-history',
       Route: '/evals'
     },
     {
--- maxmobility/apps/eval-mobile/app/modules/training/training.component.ts
+++ maxmobility/apps/eval-mobile/app/modules/training/training.component.ts
@@ -1,6 +1,9 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { NavigationStart, Router } from '@angular/router';
 import { RouterExtensions } from 'nativescript-angular/router';
+import { Subscription } from 'rxjs';
+import { Page } from 'tns-core-modules/ui/page';
 import { SnackBar } from 'nativescript-snackbar';
 import { isAndroid, isIOS } from 'tns-core-modules/platform';
 const carousel = require('nativescript-carousel').Carousel;
@@ -12,11 +15,20 @@ const carousel = require('nativescript-carousel').Carousel;
   styleUrls: ['./training.component.css']
 })
 export class TrainingComponent implements AfterViewInit {
-  @ViewChild('carousel') carousel: ElementRef;
+  @ViewChild('carousel')
+  carousel: ElementRef;
   snackbar = new SnackBar();
   slides = this.translateService.instant('training');
 
-  constructor(private routerExtensions: RouterExtensions, private translateService: TranslateService) {
+  private routeSub: Subscription; // subscription to route observer
+  private snackbarTimeoutID: any;
+
+  constructor(
+    private _page: Page,
+    private _router: Router,
+    private routerExtensions: RouterExtensions,
+    private translateService: TranslateService
+  ) {
     // re-update slides every time it's created
     this.slides = this.translateService.instant('training');
   }
@@ -39,9 +51,30 @@ export class TrainingComponent implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-    setTimeout(() => {
-      this.snackbar.simple('Swipe left to view more slides.');
+    this.snackbarTimeoutID = setTimeout(() => {
+      try {
+        this.snackbar.simple('Swipe left to view more slides.').catch(err => {
+          this.snackbarTimeoutID = null;
+        });
+      } catch (ex) {
+        this.snackbarTimeoutID = null;
+      }
+      this.snackbarTimeoutID = null;
     }, 1000);
+
+    this.routeSub = this._router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        if (this.snackbarTimeoutID) {
+          clearTimeout(this.snackbarTimeoutID);
+        }
+      }
+    });
+
+    this._page.on(Page.navigatingFromEvent, event => {
+      if (this.snackbarTimeoutID) {
+        clearTimeout(this.snackbarTimeoutID);
+      }
+    });
   }
 
   onBack(): void {
