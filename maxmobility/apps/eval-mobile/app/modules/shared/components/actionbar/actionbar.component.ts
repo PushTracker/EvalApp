153c1e85367389f0bc6d8860c6ad8ad01728c7b7
William Emfinger <waemfinger@gmail.com>
2018-11-29 17:30:03 -0600

    WIP #301 working event handling for all pages and navigation showing PT connectivity status.

--- maxmobility/apps/eval-mobile/app/modules/shared/components/actionbar/actionbar.component.ts
+++ maxmobility/apps/eval-mobile/app/modules/shared/components/actionbar/actionbar.component.ts
@@ -30,32 +30,8 @@ export class ActionbarComponent extends ActionBar {
     private _zone: NgZone
   ) {
     super();
-  }
-
-  ngOnInit() {
     this.onPushTrackerStateChange(null);
     this.register();
-
-    // see https://github.com/NativeScript/nativescript-angular/issues/1049
-    this.routeSub = this._router.events.subscribe(event => {
-      if (event instanceof NavigationStart) {
-        console.log('NAVIGATION START');
-        this.unregister();
-      }
-      if (event instanceof NavigationEnd) {
-        console.log('NAVIGATION END', event);
-        this.register();
-      }
-    });
-
-    this._page.on(Page.navigatingFromEvent, event => {
-      console.log('NAVIGATING FROM');
-      this.unregister();
-    });
-    this._page.on(Page.navigatingToEvent, event => {
-      console.log('NAVIGATING TO');
-      this.register();
-    });
   }
 
   onPTConnTap(): void {
@@ -65,12 +41,11 @@ export class ActionbarComponent extends ActionBar {
   onPushTrackerStateChange(args: any) {
     this._zone.run(() => {
       this.status = BluetoothService.pushTrackerStatus.get('state');
-      //console.log('status', this.status);
+      console.log('status', this.status);
     });
   }
 
   unregister(): void {
-    console.log('actionbar unregistering');
     BluetoothService.pushTrackerStatus.removeEventListener(
       Observable.propertyChangeEvent,
       this.onPushTrackerStateChange
@@ -78,8 +53,7 @@ export class ActionbarComponent extends ActionBar {
   }
 
   register(): void {
-    this.unregister();
-    console.log('actionbar registering');
+    //this.unregister();
     BluetoothService.pushTrackerStatus.addEventListener(
       Observable.propertyChangeEvent,
       this.onPushTrackerStateChange,
