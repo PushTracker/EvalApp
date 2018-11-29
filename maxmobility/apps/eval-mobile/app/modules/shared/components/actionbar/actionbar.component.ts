3a546290618626d5c26be982ce64564770e114ba
William Emfinger <waemfinger@gmail.com>
2018-11-29 17:15:02 -0600

    WIP #301 trying to get status to update even after back navigation.

--- maxmobility/apps/eval-mobile/app/modules/shared/components/actionbar/actionbar.component.ts
+++ maxmobility/apps/eval-mobile/app/modules/shared/components/actionbar/actionbar.component.ts
@@ -4,8 +4,9 @@ import { ActionBar, ActionItem } from 'ui/action-bar';
 
 import { BluetoothService } from '@maxmobility/mobile';
 
-import { NavigationStart, Router } from '@angular/router';
+import { NavigationStart, NavigationEnd, Router } from '@angular/router';
 import { Page } from 'tns-core-modules/ui/page';
+import { Subscription } from 'rxjs';
 
 import { Observable, fromObject } from 'tns-core-modules/data/observable';
 
@@ -29,24 +30,32 @@ export class ActionbarComponent extends ActionBar {
     private _zone: NgZone
   ) {
     super();
-    this.onPushTrackerStateChange(null);
-    this.register();
   }
 
   ngOnInit() {
-    /*
+    this.onPushTrackerStateChange(null);
+    this.register();
+
     // see https://github.com/NativeScript/nativescript-angular/issues/1049
     this.routeSub = this._router.events.subscribe(event => {
       if (event instanceof NavigationStart) {
+        console.log('NAVIGATION START');
         this.unregister();
+      }
+      if (event instanceof NavigationEnd) {
+        console.log('NAVIGATION END', event);
+        this.register();
       }
     });
 
     this._page.on(Page.navigatingFromEvent, event => {
+      console.log('NAVIGATING FROM');
       this.unregister();
     });
-
-	  */
+    this._page.on(Page.navigatingToEvent, event => {
+      console.log('NAVIGATING TO');
+      this.register();
+    });
   }
 
   onPTConnTap(): void {
@@ -61,6 +70,7 @@ export class ActionbarComponent extends ActionBar {
   }
 
   unregister(): void {
+    console.log('actionbar unregistering');
     BluetoothService.pushTrackerStatus.removeEventListener(
       Observable.propertyChangeEvent,
       this.onPushTrackerStateChange
@@ -69,6 +79,7 @@ export class ActionbarComponent extends ActionBar {
 
   register(): void {
     this.unregister();
+    console.log('actionbar registering');
     BluetoothService.pushTrackerStatus.addEventListener(
       Observable.propertyChangeEvent,
       this.onPushTrackerStateChange,
