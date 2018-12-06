18c6937b7ca9b7eafd0e3098f7b903d175c3b536
William Emfinger <waemfinger@gmail.com>
2018-12-06 10:33:22 -0600

    updated template to use ngIf to allow the keyboard to scroll properly.

--- maxmobility/apps/eval-mobile/app/modules/sign-up/sign-up.module.ts
+++ maxmobility/apps/eval-mobile/app/modules/sign-up/sign-up.module.ts
@@ -5,12 +5,18 @@ import { NativeScriptRouterModule } from 'nativescript-angular/router';
 import { NativeScriptFormsModule } from 'nativescript-angular/forms';
 // app
 import { TranslateModule } from '@ngx-translate/core';
+import { SharedModule } from '../shared/shared.module';
 import { SignUpComponent } from './sign-up.component';
 
 const routes: Routes = [{ path: '', component: SignUpComponent }];
 
 @NgModule({
-  imports: [NativeScriptRouterModule.forChild(routes), NativeScriptFormsModule, TranslateModule],
+  imports: [
+    NativeScriptRouterModule.forChild(routes),
+    SharedModule,
+    NativeScriptFormsModule,
+    TranslateModule
+  ],
   declarations: [SignUpComponent],
   schemas: [NO_ERRORS_SCHEMA]
 })
