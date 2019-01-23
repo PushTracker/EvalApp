2f33d79f64169dca0bc460c3233f3db9dd207520
bradmartin <bradwaynemartin@gmail.com>
2019-01-23 10:56:29 -0600

    demo-requests model

---
+++ maxmobility/libs/core/models/demo-request.ts
@@ -0,0 +1,35 @@
+export class DemoRequest {
+  _id: string;
+  _acl: { creator: string };
+  _kmd: { ect: string; lmt: string };
+
+  /**
+   * When creating a request, the user can input contact information which helps the user providing the demo unit know who to contact.
+   */
+  contact_info: string;
+
+  /**
+   * When a demo request is complete, the rep will mark it complete.
+   */
+  complete: boolean;
+
+  /**
+   * The rep who "claimed" the demo request. This will flag the record so data can be filtered.
+   */
+  claimed_user: string;
+
+  /**
+   * User requesting the demo.
+   */
+  user: string;
+
+  /**
+   * Location of the user requesting demo. Useful to determine the distance radius for sending push notifications.
+   */
+  _geoloc: [number, number];
+
+  /**
+   * Max Distance in miles to contact reps via push notification that a demo has been requested.
+   */
+  maxDistance: number;
+}
