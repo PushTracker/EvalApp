455b50534e894b303dd4c87128403f2a32c77634
Brad Martin <bradwaynemartin@gmail.com>
2019-01-31 10:43:41 -0600

    removes underscore dep usage

--- maxmobility/libs/core/models/demo.model.ts
+++ maxmobility/libs/core/models/demo.model.ts
@@ -4,7 +4,6 @@ import { Kinvey } from 'kinvey-nativescript-sdk';
 import * as LS from 'nativescript-localstorage';
 import { Observable } from 'tns-core-modules/data/observable';
 import * as imageSource from 'tns-core-modules/image-source';
-import * as _ from 'underscore';
 
 export class Record extends Observable {
   time: Date;
@@ -113,11 +112,19 @@ export class Demo extends Observable {
   }
 
   get version_string(): string {
-    return [this.pt_version_string, this.mcu_version_string, this.ble_version_string].join(', ');
+    return [
+      this.pt_version_string,
+      this.mcu_version_string,
+      this.ble_version_string
+    ].join(', ');
   }
 
   get pt_version_string(): string {
-    if (this.pt_version && this.pt_version.length && this.pt_version.includes('.')) {
+    if (
+      this.pt_version &&
+      this.pt_version.length &&
+      this.pt_version.includes('.')
+    ) {
       return this.pt_version.trim();
     } else {
       return '??';
@@ -125,7 +132,11 @@ export class Demo extends Observable {
   }
 
   get ble_version_string(): string {
-    if (this.ble_version && this.ble_version.length && this.ble_version.includes('.')) {
+    if (
+      this.ble_version &&
+      this.ble_version.length &&
+      this.ble_version.includes('.')
+    ) {
       return this.ble_version.trim();
     } else {
       return '??';
@@ -133,7 +144,11 @@ export class Demo extends Observable {
   }
 
   get mcu_version_string(): string {
-    if (this.mcu_version && this.mcu_version.length && this.mcu_version.includes('.')) {
+    if (
+      this.mcu_version &&
+      this.mcu_version.length &&
+      this.mcu_version.includes('.')
+    ) {
       return this.mcu_version.trim();
     } else {
       return '??';
@@ -161,7 +176,11 @@ export class Demo extends Observable {
       })
     };
     Object.keys(this).map(k => {
-      if (typeof this[k] === 'number' || typeof this[k] === 'string' || typeof this[k] === 'boolean') {
+      if (
+        typeof this[k] === 'number' ||
+        typeof this[k] === 'string' ||
+        typeof this[k] === 'boolean'
+      ) {
         obj[k] = this[k];
       }
     });
@@ -260,7 +279,9 @@ export class Demo extends Observable {
 
   versionsAreEqualTo(version: string): boolean {
     return (
-      this.pt_version.includes(version) && this.mcu_version.includes(version) && this.ble_version.includes(version)
+      this.pt_version.includes(version) &&
+      this.mcu_version.includes(version) &&
+      this.ble_version.includes(version)
     );
   }
 
@@ -286,7 +307,10 @@ export class Demo extends Observable {
       const bDate = b.getTime();
       return aDate < bDate ? 1 : -1;
     });
-    this.usage = _.uniq(this.usage, true, o => o.getTime().toISOString());
+
+    this.usage = Array.from(
+      new Set(this.usage.map(item => item.getTime().toISOString()))
+    ) as any;
   }
 
   use(): Promise<any> {
