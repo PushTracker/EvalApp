1a84afaf9df7c5945ddf2878b32f8f65c6176987
Brad Martin <bradwaynemartin@gmail.com>
2019-02-26 17:13:29 -0600

    root ref.d.ts

--- maxmobility/apps/eval-mobile/references.d.ts
+++ maxmobility/apps/eval-mobile/references.d.ts
@@ -1,4 +1,2 @@
-declare module 'nativescript-worker-loader!*' {
-  const content: any;
-  export = content;
-}
+/// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
+/// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
