bbc70f05d1c7e3dd7086c6b69d1e4bbce765a5a9
Brad Martin <bradwaynemartin@gmail.com>
2019-02-27 22:16:38 -0600

    fix imports

--- maxmobility/apps/eval-mobile/app/main.ts
+++ maxmobility/apps/eval-mobile/app/main.ts
@@ -1,14 +1,19 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScriptDynamic } from 'nativescript-angular/platform';
-// import global stuff here
 import 'nativescript-localstorage'; // for local/session storage plugin
-// util functions (https://github.com/NathanaelA/nativescript-master-technology)
-import 'nativescript-master-technology';
-import * as app from 'tns-core-modules/application';
-
-import { AppModule } from './app.module';
+import 'nativescript-master-technology'; // util functions (https://github.com/NathanaelA/nativescript-master-technology)
+import 'reflect-metadata';
 import './async-await';
+import * as app from 'tns-core-modules/application';
+import { enableProdMode } from '@angular/core';
 import { BluetoothService } from '@maxmobility/mobile';
+import { AppModule } from './app.module';
+
+// If built with env.uglify
+declare const __UGLIFIED__;
+if (typeof __UGLIFIED__ !== 'undefined' && __UGLIFIED__) {
+  enableProdMode();
+}
 
 // Overriding the app delegate background enter to create a background task so that if we are performing OTAs
 // it will continue for the duration of iOS allows
@@ -17,12 +22,10 @@ if (app.ios) {
   const Del = (UIResponder as any).extend(
     {
       applicationDidEnterBackground(application: UIApplication): void {
-        console.log('applicationDidEnterBackground', application);
         BluetoothService.requestOtaBackgroundExecution();
       },
 
       applicationDidBecomeActive(application: UIApplication): void {
-        console.log('applicationDidBecomeActive', application);
         BluetoothService.stopOtaBackgroundExecution();
       }
     },
